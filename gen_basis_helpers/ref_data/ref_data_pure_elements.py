bee48785a078f8868f0c7e51afb30e5265b59147
Richard M Fogarty <richard.m.fogarty@gmail.com>
2019-11-06 18:36:56 +0000

    ref_data: Added separate data object for the mcweda calcs (allows me to use a diff pair-pot in this case)

--- gen_basis_helpers/ref_data/ref_data_pure_elements.py
+++ gen_basis_helpers/ref_data/ref_data_pure_elements.py
@@ -2,6 +2,8 @@
 """ Combines data for various pure-elements into a small number of objects. Effectively the interface module """
 
 from types import SimpleNamespace
+import os
+import plato_pylib.plato.plato_paths as platoPaths
 from . import ref_elemental_objs as refEleObjs
 from . import ref_data_mg as refMg
 from . import ref_data_zr as refZr
@@ -14,5 +16,16 @@ shellAngMomMaps = refEleObjs.ShellAngMomMapper({"Mg": refMg.createMgAngMomShellI
                                                 "Zr": refZr.createZrAngMomShellIndices()})
 
 
+#Swap all datasets to use the McWeda models
+elementalDataMcWeda = SimpleNamespace(Mg=refMg.createMgReferenceDataObj(),
+                                Zr=refZr.createZrReferenceDataObj())
 
+
+elementalDataMcWeda.Mg.modelFiles.tb1Model = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Mg_models","three_body_2019") )
+elementalDataMcWeda.Mg.modelFiles.dft2Model = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Mg_models","three_body_2019") )
+elementalDataMcWeda.Mg.modelFiles.dftModel = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Mg_models","three_body_2019"), dtype="dft")
+
+elementalDataMcWeda.Mg.modelFiles.tb1Model = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Zr_models","three_body_2019") )
+elementalDataMcWeda.Mg.modelFiles.dft2Model = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Zr_models","three_body_2019") )
+elementalDataMcWeda.Mg.modelFiles.dftModel = platoPaths.getAbsolutePathForPlatoTightBindingDataSet( os.path.join("Zr_models","three_body_2019"), dtype="dft")
 
