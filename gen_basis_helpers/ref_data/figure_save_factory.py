95dfc544c7093d387074c99cd6d41838b76e672f
Richard M Fogarty <richard.m.fogarty@gmail.com>
2019-11-25 10:24:17 +0000

    ref_data: Added figure saver for 3-centre tb paper (also mrs-boston 2019 talk figure saver)

--- gen_basis_helpers/ref_data/figure_save_factory.py
+++ gen_basis_helpers/ref_data/figure_save_factory.py
@@ -1,4 +1,6 @@
 
+import os
+
 from . import figure_savers_base as baseObjs 
 from ..shared import config_vars as configVars
 
@@ -7,4 +9,11 @@ def createFigureSaver_twoCentreTightBindingPaper():
 	baseFolder = configVars.TWO_CENTRE_PAPER_2019_FIG_SAVE_FOLDER
 	return baseObjs.StandardFigureSaver.fromDefaultPlusKwargs(baseFolder=baseFolder)
 
+def createFigureSaver_threeCentreTightBindingPaper():
+	baseFolder = configVars.THREE_CENTRE_PAPER_2019_FIG_SAVE_FOLDER
+	return baseObjs.StandardFigureSaver.fromDefaultPlusKwargs(baseFolder=baseFolder)
 
+def createFigureSaver_threeCentreTightBindingBostomTalk():
+	outSaver = createFigureSaver_threeCentreTightBindingPaper()
+	outSaver.baseFolder = os.path.join( outSaver.baseFolder, "mrs_talk" )
+	return outSaver
