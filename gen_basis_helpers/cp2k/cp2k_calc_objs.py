54920849113d4a7242ade338b3c84280c58f1668
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-02-22 10:19:37 +0000

    cp2k: Added optional descriptors for max-scf cycles and grid cutoffs to CP2K method object

--- gen_basis_helpers/cp2k/cp2k_calc_objs.py
+++ gen_basis_helpers/cp2k/cp2k_calc_objs.py
@@ -1,22 +1,25 @@
 import os
+import pathlib
+import types
 
 import plato_pylib.parseOther.parse_cp2k_files as parseCP2K
 
-from ..shared import methodObjs
-
+from ..shared import method_objs as methodObjs
+from . import cp2k_file_helpers as pyCP2KHelpers
 
 class CP2KCalcObj(methodObjs.CalcMethod):
+
 	def __init__(self, pycp2kObj, basePath=None):
 		self.cp2kObj = pycp2kObj
 		if basePath is None:
 			self.basePath = os.path.abspath( os.path.join( os.getcwd(), "cp2k_file" ) )
 		else:
 			self.basePath = os.path.abspath( os.path.splitext(basePath)[0] )
-		
 	
 	def writeFile(self):
 		self.cp2kObj.project_name = os.path.split(self.basePath)[1]
 		self.cp2kObj.working_directory = os.path.split(self.basePath)[0]
+		pathlib.Path(self.cp2kObj.working_directory).mkdir(parents=True,exist_ok=True)
 		self.cp2kObj.write_input_file()
 		
 	@property
@@ -39,11 +42,7 @@ class CP2KCalcObj(methodObjs.CalcMethod):
 	@property
 	def parsedFile(self):
 		parsedDict = parseCP2K.parseCpout(self.outFilePath)
-		return SimpleNameSpace(**parsedDict)
-
-
-
-
+		return types.SimpleNamespace(**parsedDict)
 
 #Optional descriptors that can be added
 def addInpPathDescriptorToCP2KCalcObjCLASS(inpCls):
@@ -53,10 +52,24 @@ def addInpPathDescriptorToCP2KCalcObjCLASS(inpCls):
 def addQuickStepEpsDefDescriptorToCP2KCalcObjCLASS(inpCls, attrName="qs_eps_def"):
 	setattr(inpCls, attrName, QuickStepEpsDefaultCP2K(attrName))
 
+def addRelGridDescriptorToCP2KCalcObjCLASS(inpCls, attrName="relGridCutoff"):
+	units = "eV"
+	setattr(inpCls, attrName, RelGridDescriptorCP2K(attrName,units))
+
+def addAbsGridDescriptorToCP2KCalcObjCLASS(inpCls, attrName="absGridCutoff"):
+	units="eV"
+	setattr(inpCls, attrName, AbsGridDescriptorCP2K(attrName,units))
+
+def addMaxScfDescriptorToCP2KCalcObjCLASS(inpCls, attrName="maxScf"):
+	setattr(inpCls, attrName, MaxScfDescriptorCP2K(attrName) )
+
 #Defining an inpPath property for CP2KCalcObj
+#Note - it is VERY HARD to include a docsting on these (id need to return a special class with a repr well defined instead of a str from get, the docstirng will be accessed on the return class
 class InpPathDescriptorCP2K(object): 
+
 	def __init__(self, attrName):
 		self.name = attrName
+
 	def __get__(self, instance, owner):
 		return instance.basePath + ".inp"
   
@@ -65,6 +78,7 @@ class InpPathDescriptorCP2K(object):
 
 #Defining quickstep_eps_default parameter for CP2KCalcObj
 class QuickStepEpsDefaultCP2K(object):
+
 	def __init__(self,attrName):
 		self.name = attrName
 
@@ -74,4 +88,68 @@ class QuickStepEpsDefaultCP2K(object):
 	def __set__(self, instance, value):
 		assert( len(instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list) == 1 )
 		instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].DFT.QS.Eps_default = value
+
+#Defining relative grid spacing in the CP2K object
+class RelGridDescriptorCP2K():
+
+	def __init__(self, attrName, units):
+		self.name = attrName
+		self.units = units.lower()
+
+	def __get__(self, instance, owner):
+		strVal = instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].DFT.MGRID.Rel_cutoff
+		self._checkUnitsCorrect(strVal)
+		floatVal = float(strVal.strip().split()[-1])
+		return floatVal
+
+	#We can probably ALWAYS insist the units are in eV, since that makes it easier to our acceptor. If user wants difference
+	#units for the interface we can convert them internally (will implement if ever actually needed)
+	def _checkUnitsCorrect(self,strVal):
+		assert self.units=="ev", "For now only ev units are supported for RelGridDescriptor"
+		assert "ev" in strVal.lower(), "Units eV need to be present in relative cutoff str:{}".format(strVal) 
+			
+	#Note this can only set in eV for now. Easiest modificatoin is probably to always convert units if others requested
+	def __set__(self, instance, value):
+		assert( len(instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list) == 1 )
+		modDict = {"gridCutRel":value}
+		pyCP2KHelpers.modCp2kObjBasedOnDict(instance.cp2kObj, modDict)
+
+class AbsGridDescriptorCP2K():
+
+	def __init__(self, attrName, units):
+		self.name = attrName
+		self.units = units.lower()
+
+	def __get__(self, instance, owner):
+		strVal = instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].DFT.MGRID.Cutoff
+		self._checkUnitsCorrect(strVal)
+		floatVal = float(strVal.strip().split()[-1])
+		return floatVal
+
+	def __set__(self, instance, value):
+		assert( len(instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list) == 1 )
+		modDict = {"gridCutAbs":value}
+		pyCP2KHelpers.modCp2kObjBasedOnDict(instance.cp2kObj, modDict)
+
+	def _checkUnitsCorrect(self,strVal):
+		assert self.units=="ev", "For now only ev units are supported for RelGridDescriptor"
+		assert "ev" in strVal.lower(), "Units eV need to be present in relative cutoff str:{}".format(strVal) 
+
+
+class MaxScfDescriptorCP2K():
+
+	def __init__(self, attrName):
+		self.name = attrName
+
+
+	def __get__(self, instance, owner):
+		strVal = instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].DFT.SCF.Max_scf
+		outVal = int( strVal.strip().split()[-1] )
+		return outVal
+
+	def __set__(self, instance, value):
+		assert( len(instance.cp2kObj.CP2K_INPUT.FORCE_EVAL_list) == 1 )
+		modDict = {"maxscf":value}
+		pyCP2KHelpers.modCp2kObjBasedOnDict(instance.cp2kObj, modDict)
+
 
---
+++ gen_basis_helpers/cp2k/unit_tests/utests_calc_objs.py
@@ -0,0 +1,68 @@
+
+import copy
+
+import unittest
+import unittest.mock as mock
+
+import gen_basis_helpers.cp2k.cp2k_calc_objs as tCode
+import gen_basis_helpers.cp2k.method_register as methReg
+
+
+class DudCP2KObj(tCode.CP2KCalcObj):
+	""" Point of this is to allow me to test the actual class without interfering with it (e.g. through class decorators) for other test modules """
+	pass
+
+class TestCP2KCalcObjGridDescriptors(unittest.TestCase):
+
+	def setUp(self):
+		tCode.addRelGridDescriptorToCP2KCalcObjCLASS(DudCP2KObj)
+		tCode.addAbsGridDescriptorToCP2KCalcObjCLASS(DudCP2KObj)
+		self.pyCP2KObjA = methReg.createCP2KObjFromMethodStr("cp2k_test_object")
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = DudCP2KObj( self.pyCP2KObjA )
+
+	def testAddingRelGridDescriptorGivesConsistentGetterAndSetter(self):
+		startVal = copy.deepcopy( self.testObjA.relGridCutoff )
+		testVal = 500
+		self.assertTrue ( abs(testVal-startVal) > 0.01 ) #Test that our starting value is far from our test value
+		self.testObjA.relGridCutoff = testVal
+		actVal = self.testObjA.relGridCutoff
+		self.assertAlmostEqual(testVal, actVal)
+
+	def testAssertErrorThrownIfRelGridInitUnitsNotInEV(self):
+		self.pyCP2KObjA.CP2K_INPUT.FORCE_EVAL_list[-1].DFT.MGRID.Rel_cutoff = "[Ry] 500"
+		with self.assertRaises(AssertionError):
+			unused = self.testObjA.relGridCutoff
+
+	def testAddingAbsGridDescriptorGivesConsistentGetterAndSetter(self):
+		startVal = copy.deepcopy( self.testObjA.absGridCutoff )
+		testVal = 500
+		self.assertTrue ( abs(testVal-startVal) > 0.01 ) #Test that our starting value is far from our test value
+		self.testObjA.absGridCutoff = testVal
+		actVal = self.testObjA.absGridCutoff
+		self.assertAlmostEqual(testVal, actVal)
+
+
+class TestCP2KMaxScfDescriptor(unittest.TestCase):
+
+	def setUp(self):
+		self.pyCP2KObjA = methReg.createCP2KObjFromMethodStr("cp2k_test_object")
+		tCode.addMaxScfDescriptorToCP2KCalcObjCLASS(DudCP2KObj)
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = DudCP2KObj( self.pyCP2KObjA )
+
+	def testGetterAndSetterConsistent(self):
+		origMaxCycles = self.testObjA.maxScf
+		testMaxCycles = 24
+		self.assertNotEqual(origMaxCycles, testMaxCycles)
+		self.testObjA.maxScf = testMaxCycles
+		actMaxCycles = self.testObjA.maxScf
+		self.assertEqual(testMaxCycles, actMaxCycles)
+
+
+
+
