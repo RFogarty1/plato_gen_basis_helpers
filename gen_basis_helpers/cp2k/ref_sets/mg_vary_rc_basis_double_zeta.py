dcb8c292081975f78dbade1f03146d4f16ecd11a
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-03-18 10:35:17 +0000

    cp2k: Registered single zeta plato basis sets

--- gen_basis_helpers/cp2k/ref_sets/mg_vary_rc_basis_double_zeta.py
+++ gen_basis_helpers/cp2k/ref_sets/mg_vary_rc_basis_double_zeta.py
@@ -7,6 +7,23 @@ from .. import basis_register as basRegister
 from .. import cp2k_basis_obj as basObj
 
 
+#Single-zeta basis sets (despite the file name)
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("GTH-PBE-q2-spd-1z-rc7pt0-r05pt5-1".lower())
+def _getMgRc7pt0BasisObj_singleZeta():
+    return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="spd-1z-rc7pt0-r05pt5-1",
+                                 potential="GTH-PBE-q2", potFile="GTH_POTENTIALS", basisFile="PLATO_BASIS" )
+
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("GTH-PBE-q2-spd-1z-rc7pt5-r06pt0-1".lower())
+def _getMgRc7pt5BasisObj_singleZeta():
+	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="spd-1z-rc7pt5-r06pt0-1",
+	                             potential="GTH-PBE-q2", potFile="GTH_POTENTIALS", basisFile="PLATO_BASIS" )
+
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("GTH-PBE-q2-spd-1z-rc8pt0-r06pt5-1".lower())
+def _getMgRc8pt0BasisObj_singleZeta():
+	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="spd-1z-rc8pt0-r06pt5-1",
+	                             potential="GTH-PBE-q2", potFile="GTH_POTENTIALS", basisFile="PLATO_BASIS" )
+
+
 #Rc=7.0 basis set
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("GTH-PBE-q2-spd-2z-rc7pt0-r05pt5-1".lower())
 def _getMgRc7pt0BasisObj():
