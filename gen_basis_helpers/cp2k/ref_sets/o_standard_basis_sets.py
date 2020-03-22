203d467bac25606d9852712f9748fe0a64382a81
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-03-22 11:02:01 +0000

    cp2k: Fixed standard oxygen basis sets
    They pointed to the MOLOPT_UCL rather than plain MOLOPT basis set file

--- gen_basis_helpers/cp2k/ref_sets/o_standard_basis_sets.py
+++ gen_basis_helpers/cp2k/ref_sets/o_standard_basis_sets.py
@@ -8,29 +8,29 @@ from .. import cp2k_basis_obj as basObj
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("O-GTH-PBE-q6-MolOpt-UCL-sp".lower())
 def _getOxygenMolOptUclSZVBasis():
 	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="SZV-MOLOPT-GTH-q6",
-	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
 
 #13 basis functions
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("O-GTH-PBE-q6-MolOpt-UCL-ssppd".lower())
 def _getOxygenMolOptUclDZPVBasis():
 	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="DZVP-MOLOPT-GTH-q6",
-	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
 
 #17 basis functions
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("O-GTH-PBE-q6-MolOpt-UCL-ssspppd".lower())
 def _getOxygenMolOptUclTZVPBasis():
 	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="TZVP-MOLOPT-GTH-q6",
-	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
 
 #22 basis functions
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("O-GTH-PBE-q6-MolOpt-UCL-ssspppdd".lower())
 def _getOxygenMolOptUclTZV2PBasis():
 	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="TZV2P-MOLOPT-GTH-q6",
-	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
 
 #29 basis functions
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("O-GTH-PBE-q6-MolOpt-UCL-ssspppddf".lower())
 def _getOxygenMolOptUclTZV2P_plusFOribtal_Basis():
 	return basObj.CP2KBasisObjStandard( element="will_be_overwritten", basis="TZV2PX-MOLOPT-GTH-q6",
-	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+	                             potential="GTH-PBE-q6", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
 
