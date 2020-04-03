6f85e934d3f174669bfd1cd3e51b5c1265483996
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-04-03 09:47:03 +0100

    surfaces: Added rocksalt 001 surface

--- gen_basis_helpers/shared/surfaces.py
+++ gen_basis_helpers/shared/surfaces.py
@@ -2,6 +2,7 @@
 import copy
 import math
 
+import plato_pylib.shared.ucell_class as uCell
 import plato_pylib.utils.supercell as supCell
 
 import gen_basis_helpers.shared.base_surface as baseSurface
@@ -41,19 +42,7 @@ class Hcp0001Surface(baseSurface.BaseSurface):
 	def unitCell(self):
 		startCell = copy.deepcopy( self._bulkCell )
 		outBulkCell = supCell.superCellFromUCell(self._bulkCell,[1,1,self._nLayers]) 
-
-		#Need to add the vacuum, half at top and half at bottom
-		lattParams = copy.deepcopy( outBulkCell.lattParams )
-		lattParams["c"] += self._lenVac
-		cartCoords = copy.deepcopy(outBulkCell.cartCoords)
-	
-		for row in cartCoords:
-			row[2] += 0.5*self._lenVac
-
-
-		outBulkCell.lattParams = lattParams #NOTE: We MUST change lattParams first, since this changes the cart-coords of atoms
-		outBulkCell.cartCoords = cartCoords
-	
+		addVacuumToUnitCellAlongC(outBulkCell, self._lenVac)
 		return outBulkCell
 
 	@property
@@ -78,6 +67,97 @@ class Hcp0001Surface(baseSurface.BaseSurface):
 		areaSqr = p*(p-lattParams["a"])*(p-lattParams["b"])*(p-base)
 		area = math.sqrt(areaSqr)
 		return area*2 #The cell surface is made of two of these trigangles (its a parallelogram in general)
+
+
+class Rocksalt001Surface(baseSurface.BaseSurface):
+
+	def __init__(self, singleLayerCell, nLayers, lenVac):
+		self._singleLayerCell = singleLayerCell
+		self._nLayers = nLayers
+		self._lenVac = lenVac
+	
+	@property
+	def unitCell(self):
+		superCell = supCell.superCellFromUCell(self._singleLayerCell, [1,1,self._nLayers])
+		addVacuumToUnitCellAlongC(superCell, self._lenVac)
+		return superCell
+
+	@property
+	def surfaceArea(self):
+		return (self._singleLayerCell.volume / self._singleLayerCell.lattParams["c"])
+
+
+
+def addVacuumToUnitCellAlongC(inpCell, lenVac):
+	#Need to add the vacuum, half at top and half at bottom
+	lattParams = copy.deepcopy( inpCell.lattParams )
+	lattParams["c"] += lenVac
+	cartCoords = copy.deepcopy(inpCell.cartCoords)
+
+	for row in cartCoords:
+		row[2] += 0.5*lenVac
+
+	inpCell.lattParams = lattParams #NOTE: We MUST change lattParams first, since this changes the cart-coords of atoms
+	inpCell.cartCoords = cartCoords
+
+
+
+def getSingleLayerRocksalt001FromPrimitiveCell(primCell):
+	""" Description of function
+	
+	Args:
+		primCell: (plato_pylib UnitCell object) This must be a primitive rock salt cell. Meaning 2 atoms, with each lattice angle=60 degrees and a=b=c
+			 
+	Returns
+		 outCell: (plato_pylib UnitCell object) Contains a 4-atom unit-cell which forms the basis for forming rock-salt 001 surfaces (it is one layer)
+ 
+	Raises:
+		 AssertionError: If the input primitive cell is not a rock salt cell. Probably not garanteed to be raised if an incompatible cell is passed, but at least catches some errors.
+	"""
+
+	assert _uCellIsRockSaltPrimitive(primCell, printError=True), "UnitCell {} is not a primitive rock salt cell".format(primCell)
+
+	
+	#From here onwards i assume primCell is in the correct format
+	inpLattParam = primCell.lattParams["a"]
+	_tempA = 1/math.sqrt(2) #if a unit vector is halfway between x/y axes, then each component hsa this value
+	lattVects = [ [_tempA*inpLattParam,     _tempA*inpLattParam, 0],
+	              [-1*_tempA*inpLattParam, _tempA*inpLattParam, 0],
+	              [0.0                   , 0.0,                inpLattParam] ]
+	eleA, eleB = [x[-1] for x in primCell.fractCoords]
+	fractCoords = [ [0.0,0.0,0.0,eleA], [0.0,0.0,0.5,eleB], [0.5,0.5,0.0,eleB], [0.5,0.5,0.5,eleA] ]
+
+
+
+	return uCell.UnitCell.fromLattVects(lattVects, fractCoords)
+
+
+def _uCellIsRockSaltPrimitive(inpCell, printError=True, angleTol=1e-1, lattParamTol=1e-1):
+	isPrim = True
+	printMsg = ""
+
+	#Check right number of atoms
+	if len(inpCell.fractCoords) != 2:
+		printMsg = "2 atoms expected for rocksalt primitive cell, found {}".format( len(inpCell.fractCoords) )
+		isPrim = False
+
+	#Check correct angle
+	angles = [x for x in inpCell.lattAngles.values()]
+	expAngle = 60.0
+	if not all([ abs((x-expAngle))<angleTol for x in angles]):
+		printMsg = "Angles should also be 60.0 degrees for rocksalt primitive cell, found angles of {}".format(angles)
+		isPrim=False
+
+	#Check correct lattice paramter relationships
+	lattParams = [x for x in inpCell.lattParams.values()]
+	if not all([ abs(x-lattParams[0]) < lattParamTol for x in lattParams]):
+		printMsg = "Lattice parameters should all be equal for rocksalt primitive cell, found parameters of {}".format(lattParams)
+		isPrim=False
+
+	if printError:
+		print(printMsg)
+
+	return isPrim
 
 
 
--- gen_basis_helpers/shared/unit_tests/utests_surface_impls.py
+++ gen_basis_helpers/shared/unit_tests/utests_surface_impls.py
@@ -3,7 +3,11 @@
 """ Test specific implementations of surface objects """
 
 import copy
+import itertools as it
+import math
 import unittest
+import unittest.mock as mock
+
 import numpy as np
 import plato_pylib.shared.ucell_class as UCell
 
@@ -70,6 +74,115 @@ class TestHcp001Surface(unittest.TestCase):
 		expectedNumbAtoms = 6
 		actNumbAtoms = len( self.testObj.unitCell.cartCoords )
 		self.assertEqual(expectedNumbAtoms, actNumbAtoms)
+
+
+
+class TestBuildRocksalt001SingleLayerFromPrimitive(unittest.TestCase):
+
+	def setUp(self):
+		self.a = 4
+		self.b = 4
+		self.c = 4
+		self.alpha = 60
+		self.beta = 60
+		self.gamma = 60
+		self.eleA = "Mg"
+		self.eleB = "O"
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.fractCoordsA = [ [0.0,0.0,0.0,self.eleA], [0.5,0.5,0.5,self.eleB] ]
+		self.testCellA = UCell.UnitCell( lattParams=[self.a,self.b,self.c], lattAngles=[self.alpha,self.beta,self.gamma])
+		self.testCellA.fractCoords = self.fractCoordsA #Unfortunately cant set this way upon initialization; in that case coords and eles need to be in separate iterables
+
+	def _runFunct(self):
+		return tCode.getSingleLayerRocksalt001FromPrimitiveCell(self.testCellA)
+
+	@mock.patch("gen_basis_helpers.shared.surfaces.uCell.UnitCell")
+	def testExpectedUCellCallForTestCellA(self, mockedUCellClass):
+		expFractCoords = [ [0,0,0,self.eleA], [0,0,0.5,self.eleB], [0.5,0.5,0,self.eleB], [0.5,0.5,0.5,self.eleA] ]
+		expLattVects = [ [ (1/math.sqrt(2))*self.a   , (1/math.sqrt(2))*self.b, 0.0   ],
+		                 [ -1*(1/math.sqrt(2))*self.a, (1/math.sqrt(2))*self.b, 0.0   ],
+		                 [ 0                         , 0                      , self.c] ]
+
+		mockOutput = mock.Mock()
+		mockedUCellClass.fromLattVects.side_effect = [mockOutput]
+
+		actOutput = self._runFunct()
+		inpArgs,kwargs = mockedUCellClass.fromLattVects.call_args
+		actLattVects,actFractCoords = inpArgs
+
+		#Check expected lattice vectors passed
+		for exp,act in it.zip_longest(expLattVects, actLattVects):
+			[self.assertAlmostEqual(e,a) for e,a in it.zip_longest(exp,act)]
+
+		#Check expected fractional coordinates passed
+		for exp,act in it.zip_longest(expFractCoords, actFractCoords):
+			[self.assertAlmostEqual(e,a) for e,a in it.zip_longest(exp[:3],act[:3])]
+			self.assertEqual(exp[-1], act[-1])
+
+		self.assertEqual(mockOutput,actOutput)
+
+	def testRaisesForWrongNumberAtoms(self):
+		testCoords = self.testCellA.fractCoords
+		testCoords.append( [0.0,0.0,0.0,"O"] )
+		self.testCellA.fractCoords = testCoords
+		self.assertTrue( len(self.testCellA.fractCoords)!=2 )
+		with self.assertRaises(AssertionError):
+			self._runFunct()
+
+
+	def testRaisesForWrongAngles(self):
+		self.alpha, self.beta, self.gamma = 90,90,90
+		self.createTestObjs()
+		with self.assertRaises(AssertionError):
+			self._runFunct()
+
+	def testRaisesForNonCubicLattParams(self):
+		self.c = self.b*2 #Should all be equal for primitive rocksalt unit cell
+		self.createTestObjs()
+		with self.assertRaises(AssertionError):
+			self._runFunct()
+
+
+
+class TestRocksalt001Surface(unittest.TestCase):
+
+	def setUp(self):
+		self.eleA = "Mg"
+		self.eleB = "O"
+		self.a = 5.0 #Only need 1 lattice vector to start since its cubic
+		self.lenVac = 10.0
+		self.nLayers = 3
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.fractCoordsA = [ [0,0,0,self.eleA], [0,0,0.5,self.eleB], [0.5,0.5,0,self.eleB], [0.5,0.5,0.5,self.eleA] ]
+		self.lattVectsA =  [ [ (1/math.sqrt(2))*self.a   , (1/math.sqrt(2))*self.a, 0.0   ],
+		                     [ -1*(1/math.sqrt(2))*self.a, (1/math.sqrt(2))*self.a, 0.0   ],
+		                     [ 0                         , 0                      , self.a] ]
+		self.singleLayerCellA = UCell.UnitCell.fromLattVects(self.lattVectsA, self.fractCoordsA)
+
+		self.testObjA = tCode.Rocksalt001Surface( self.singleLayerCellA, self.nLayers, self.lenVac )
+
+
+	def testExpectedLattParams(self):
+		expLattAParam, expLattBParam = self.a, self.a
+		expLattCParam = (self.nLayers*self.a) + self.lenVac
+		expLattParams = {"a":expLattAParam, "b":expLattBParam, "c":expLattCParam}
+
+		outUCell = self.testObjA.unitCell
+		actLattParams = outUCell.lattParams
+
+		for key in expLattParams.keys():
+			exp,act = expLattParams[key], actLattParams[key]
+			self.assertAlmostEqual(exp,act)
+
+	def testExpSurfaceArea(self):
+		expSurfArea = self.singleLayerCellA.volume / self.a #Surface area is independent of nLayers and lenVacuum
+		actSurfArea = self.testObjA.surfaceArea
+		self.assertAlmostEqual(expSurfArea, actSurfArea)
+
 
 if __name__ == '__main__':
 	unittest.main()
