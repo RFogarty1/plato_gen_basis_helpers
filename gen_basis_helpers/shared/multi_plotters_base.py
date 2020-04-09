f19fdbeda7682f74cc8241d44d6db7153612dfbf
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-04-09 15:52:55 +0100

    multi_plotters: Added ability to add annotation to every subplot
    Main purpose is to generate a,b,c labels for paper figures

--- gen_basis_helpers/shared/multi_plotters_base.py
+++ gen_basis_helpers/shared/multi_plotters_base.py
@@ -1,4 +1,8 @@
 
+import string
+import itertools as it
+from . import misc_utils as misc
+
 """ Classes used to create figures which contain multiple plots together """
 
 
@@ -30,20 +34,119 @@ class MultiPlotGridBase():
 
 class MultiPlotterStandard(MultiPlotterBase):
 	
-	def __init__(self, plotterFactories, gridCreator):
+	def __init__(self, plotterFactories, gridCreator, annotateGraphs=False, annotateStrs=None, annotatePositionsAbs=None, annotatePositionsRel=None):
 		""" Initializer
 		
 		Args:
 			plotterFactories: (iter of DataPlotterBase objects), each has a create method which takes an axis asd a kwarg, and creates the relevant plot
 			gridCreator: (MultiPlotGridBase object) has a create method which returns (figHandle, axisHandles). These axisHandles are then populated by the plotterFactories objects
+
+		Optional Args:
+			annotateGraphs: (bool) If True annotations will be added to the graphs to label them (e.g. as a, b, c or similar)
+			annotateStrs: (iter of strs) Each element is the annotation label for one graph. If not set, then a sensible default is used
+			annotatePositionsAbs: (iter of 2-element iters OR a single 2-element iter) Determines the positions of annotateStrs on 
+			                   each plot. These are passed as the xy element to matplotlibs annotate function
+			annotatePositionsRel: (iter of 2 element iters OR a single 2-element iter) Same as above except that the positions are defined in terms relative to the x/y limits. e.g
+			                      [0.5,0.5] would mean putting it directly in the middle of the plot area
 				 
 		"""
+		#We create this dict only to check input vals are consistent
+		kwargDict = {"annotateGraphs":annotateGraphs, "annotateStrs":annotateStrs, "annotatePositionsAbs":annotatePositionsAbs, "annotatePositionsRel":annotatePositionsRel}
+		self._checkKwargDictConsistent(kwargDict)
+
 		self.plotterFactories = plotterFactories
 		self.gridCreator = gridCreator
+		self.annotateGraphs = annotateGraphs
+		self.annotateStrs =  annotateStrs if annotateStrs is not None else [x for x in string.ascii_lowercase]
+		self.annotatePositionsAbs = annotatePositionsAbs 
+		self.annotatePositionsRel = annotatePositionsRel 
 
-	def create(self):
+		if (self.annotatePositionsAbs is None) and (self.annotatePositionsRel is None):
+			self.annotatePositionsRel = [0.05,0.9]
+
+
+	def _checkKwargDictConsistent(self, kwargDict):
+		annotatePosAbs = kwargDict.get("annotatePositionsAbs",None)
+		annotatePosRel = kwargDict.get("annotatePositionsRel",None)
+
+		if ( (annotatePosAbs is not None) and (annotatePosRel is not None) ):
+			raise ValueError( ("Either annotatePositionsAbs OR annotatePositionsRel can be set; not both at once."
+			                   "Values are annotatePositionsAbs={}, annotatePositionsRel={}".format(annotatePosAbs,annotatePosRel) ) )
+
+	def create(self, **kwargs):
+		""" Create the multi-plot figure and return the handle to it. 
+
+		Kwargs are object attributes; if any are set then those values are used to create the plot but the state of this object
+		will NOT change 
+		"""
+
+		self._checkKwargDictConsistent(kwargs)
+		with misc.temporarilySetInstanceAttrs(self,kwargs):
+			outObj = self._createFromSelf()
+		return outObj
+
+	def _createFromSelf(self):
 		outFig, allAxes = self.gridCreator.create()
 		for plotFactory, axis in zip(self.plotterFactories, allAxes):
 			plotFactory.createPlot( axHandle=axis )
+
+		if self.annotateGraphs:
+			self._addAnnotations(allAxes)
 		return outFig
 
+	@property
+	def annotatePositionsAbs(self):
+		return self._annotatePositionsAbs
+
+	@annotatePositionsAbs.setter
+	def annotatePositionsAbs(self,val):
+		if val is None:
+			self._annotatePositionsAbs = None
+			return None
+		else:
+			self._annotatePositionsAbs = val
+
+		self._annotatePositionsRel = None
+
+	@property
+	def annotatePositionsRel(self):
+		return self._annotatePositionsRel
+
+	@annotatePositionsRel.setter
+	def annotatePositionsRel(self,val):
+		if val is None:
+			self._annotatePositionsRel = None
+			return None
+		else:
+			self._annotatePositionsRel = val
+		self._annotatePositionsAbs = None
+
+
+	def _getIterFromAnnotatePos(self, val):
+		try:
+			_ = val[0][0] #Will work if this is a list of 2-element iterables
+		except TypeError:
+			outVal = it.cycle([val])	#Each element needs to be a 2-element iter	
+		else:
+			outVal = it.cycle(val)
+		return outVal
+
+
+	def _addAnnotations(self, allAxes):
+		useAbsPos = True if self.annotatePositionsAbs is not None else False
+		if useAbsPos:
+			annotatePos = self.annotatePositionsAbs
+		else:
+			annotatePos = self.annotatePositionsRel
+
+		annotatePos = self._getIterFromAnnotatePos(annotatePos)
+		annotateStrs = it.cycle(self.annotateStrs)
+
+		for ax,inpStr, inpPos in zip(allAxes, annotateStrs, annotatePos):
+			if useAbsPos:
+				ax.annotate(inpStr,inpPos)
+			else:
+				currXlim, currYlim = ax.get_xlim(), ax.get_ylim()
+				xRange, yRange = currXlim[1]-currXlim[0], currYlim[1]-currYlim[0]
+				absPos = [ currXlim[0] + (xRange*inpPos[0]), currYlim[0] + (yRange*inpPos[1]) ]
+				ax.annotate(inpStr,absPos)
--- gen_basis_helpers/shared/unit_tests/utests_multi_plot_base.py
+++ gen_basis_helpers/shared/unit_tests/utests_multi_plot_base.py
@@ -1,5 +1,5 @@
 
-
+import copy
 import itertools as it
 
 import unittest
@@ -15,18 +15,90 @@ class TestMultiPlotterStandard(unittest.TestCase):
 		self.plotterA = mock.Mock()
 		self.plotterB = mock.Mock()
 		self.gridCreatorA = mock.Mock()
+		self.annotateStrs = ["a","b"]
+		self.annotateGraphs = True
+		self.annotatePositionsAbs = [0.05,0.2]
+		self.testXlimA, self.testXlimB = [0,2], [0,4]
+		self.testYlimA, self.testYlimB = [0,4], [0,6]
+
+
 		self.createTestObjs()
 
 	def createTestObjs(self):
+		#Step 1 create mock axes
+		self.mockAxes = [mock.Mock(), mock.Mock()]
+		self.gridCreatorA.create.side_effect = lambda *args, **kwargs: (mock.Mock(), self.mockAxes)
+
+		self.mockAxes[0].get_xlim.side_effect = lambda *args,**kwargs: self.testXlimA
+		self.mockAxes[1].get_xlim.side_effect = lambda *args,**kwargs: self.testXlimB
+		self.mockAxes[0].get_ylim.side_effect = lambda *args,**kwargs: self.testYlimA
+		self.mockAxes[1].get_ylim.side_effect = lambda *args,**kwargs: self.testYlimB
+
+
+
+
 		self.plotterIterA = [self.plotterA, self.plotterB]
-		self.testObjA = tCode.MultiPlotterStandard(self.plotterIterA, self.gridCreatorA)
+		self.testObjA = tCode.MultiPlotterStandard(self.plotterIterA, self.gridCreatorA, annotateGraphs=self.annotateGraphs,
+		                                           annotateStrs=self.annotateStrs, annotatePositionsAbs=self.annotatePositionsAbs)
 
 	def testStandardMultiPlotterCalledWithOutputAxes(self):
-		mockAxes =  [mock.Mock(), mock.Mock()]
-		self.gridCreatorA.create.side_effect = lambda *args, **kwargs: (mock.Mock(), mockAxes)
 		self.testObjA.create()
-		for plotFact, mockAxis in it.zip_longest(self.plotterIterA, mockAxes):
+		for plotFact, mockAxis in it.zip_longest(self.plotterIterA, self.mockAxes):
 			plotFact.createPlot.assert_called_once_with(axHandle=mockAxis)
+
+	def testExpAnnotationStrsPassed(self):
+		self.testObjA.create()
+		expStrs = self.annotateStrs
+		for mockAx, expStr in it.zip_longest(self.mockAxes, expStrs):
+			callArgs, callKwargs = mockAx.annotate.call_args
+			actStr = callArgs[0]
+			self.assertEqual(expStr,actStr)
+
+	def testExpAnnotationStrsPassedUsingInpArg(self):
+		self.annotateGraphs=False
+		self.createTestObjs()
+		self.testObjA.create(annotateGraphs=True)
+		expStrs = self.annotateStrs
+		for mockAx, expStr in it.zip_longest(self.mockAxes, expStrs):
+			callArgs, callKwargs = mockAx.annotate.call_args
+			actStr = callArgs[0]
+			self.assertEqual(expStr,actStr)
+
+	def testThrowsErrorIfSettingPositionsRelAndAbsAtSameTime(self):
+		testPosBoth = [0.1,0.1]
+		with self.assertRaises(ValueError):
+			tCode.MultiPlotterStandard(self.plotterIterA, self.gridCreatorA, annotatePositionsAbs=testPosBoth, annotatePositionsRel=testPosBoth)
+
+	def testExpPassedWhenUsingRelativePos(self):
+		testRelPos = [0.5,0.5]
+		self.createTestObjs()
+
+		#Figure out what absolute positions we want passed
+		xlimRangeA, xlimRangeB = self.testXlimA[1]-self.testXlimA[0], self.testXlimB[1]-self.testXlimB[0] 
+		ylimRangeA, ylimRangeB = self.testYlimA[1]-self.testYlimA[0], self.testYlimB[1]-self.testYlimB[0]
+		expAbsPosA = [ self.testXlimA[0] + (xlimRangeA*testRelPos[0]), self.testYlimA[0] + (ylimRangeA*testRelPos[0]) ]
+		expAbsPosB = [ self.testXlimB[0] + (xlimRangeB*testRelPos[0]), self.testYlimB[0] + (ylimRangeB*testRelPos[0]) ]
+		expAbsPositions = [expAbsPosA, expAbsPosB]
+
+		#Check actual match expected
+		self.testObjA.create(annotatePositionsRel=testRelPos)
+		for mockAx,expPos in it.zip_longest(self.mockAxes,expAbsPositions):
+			callArgs, callKwargs = mockAx.annotate.call_args
+			actPos = callArgs[1]
+			[self.assertAlmostEqual(e,a) for e,a in it.zip_longest(expPos,actPos)]
+
+
+	def testGetterAndSetterConsistentForAnnotatePosRel(self):
+		self.createTestObjs()
+		testRelPos = [0.5,0.5]
+		expVal = copy.deepcopy(testRelPos)
+		self.testObjA.annotatePositionsRel = testRelPos
+		actVal = self.testObjA.annotatePositionsRel
+		[self.assertAlmostEqual(e,a) for e,a in it.zip_longest(expVal,actVal)]
+
+
+
+
 
 
 
