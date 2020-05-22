87fd6c5fb1dbb5e50661c2b93e00e41e48b1df4a
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-05-22 10:46:00 +0100

    shared: Added class for representing planes

--- gen_basis_helpers/shared/surfaces.py
+++ gen_basis_helpers/shared/surfaces.py
@@ -279,3 +279,78 @@ def _uCellIsRockSaltPrimitive(inpCell, printError=True, angleTol=1e-1, lattParam
 
 
 
+#TODO: Move this to its own module
+class ThreeDimPlaneEquation():
+	""" Class representing a 3-dimension plane equation ax + by +cz = d
+
+	"""
+	def __init__(self, a, b, c, d):
+		""" Initializer
+		
+		Args:
+			a,b,c,d (floats): Parameters for the plane equation ax + by + cz = d
+				 
+		"""
+		self.a = a
+		self.b = b
+		self.c = c
+		self.d = d
+
+	@classmethod
+	def fromTwoPositionVectors(cls, inpVectA, inpVectB, normaliseCoeffs=True):
+		""" Alternative initializer. Creates the object using two input POSITION vectors (i.e. both MUST pass through origin)
+		
+		Args:
+			inpVectA: (len 3 float iter) Position vector
+			inpVectB: (len 3 float iter) Position vector
+			normaliseCoeffs: (Bool, Optional) If True then always return coefficients for the normalised (i.e. unit) vector normal to the plane. Default=True
+				 
+		"""
+		vectA, vectB = np.array(inpVectA), np.array(inpVectB)
+		normVect = np.cross(vectA,vectB)
+		if normaliseCoeffs:
+			lenNormVect = math.sqrt( sum([x**2 for x in normVect]) )
+			normVect = np.array( [x/lenNormVect for x in normVect] )
+
+		#D is always zero, since the position vectors are also points on the plane
+		outCoeffs = [x for x in normVect] + [0] 
+
+		return cls(*outCoeffs)
+
+
+	def getDistanceOfPointFromPlane(self, inpXyz):
+		""" Calculates the distance of a point from the plane
+		
+		Args:
+			inpXyz: (len 3 float iter) [x,y,z] co-ordinates
+				 
+		Returns
+			outDist: (float) The distance between input point and the nearest point on this plane
+	 
+		"""
+		#Step 1 = Find the d value for the parralel plane this lies on; the displacement vector between point and plane is then the vector normal to this plane
+		#Step 2  = use the fact the normal vector points the same way as the displacement vecctor to get a length
+		dValForThisPoint = self.calcDForInpXyz(inpXyz)
+		diffInDVals = abs( dValForThisPoint - self.d )
+		lenNormalVectorToThisPlane = math.sqrt( (self.a**2) + (self.b**2) + (self.c**2) )
+		outDist = diffInDVals / lenNormalVectorToThisPlane
+		return outDist
+
+
+	def calcDForInpXyz(self, inpXyz):
+		""" For a given xyz calculate d. If d=self.d then the point lies on this plane; else it lies on a parralel plane with d being the output to this function
+		
+		Args:
+			inpXyz: (len 3 float iter) co-ordinates for a point
+				 
+		"""
+		assert len(inpXyz) == 3
+		return sum( [param*coord for param,coord in it.zip_longest([self.a,self.b,self.c],inpXyz)] )
+
+
+	@property
+	def coeffs(self):
+		""" Returns a,b,c,d coeffs (in ax + by +cz = d) as a len-4 iter
+		"""
+		return [self.a,self.b,self.c,self.d]
+
--- gen_basis_helpers/shared/unit_tests/utests_surface_impls.py
+++ gen_basis_helpers/shared/unit_tests/utests_surface_impls.py
@@ -353,6 +353,63 @@ class TestCentreFractCoordsAlongC(unittest.TestCase):
 		return zCoords
 
 
+class TestThreeDimPlaneEqn(unittest.TestCase):
+
+	def setUp(self):
+		a = 3
+		b = 0
+		c = 2
+		d = 4
+		self.planeParams = [a,b,c,d]
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = tCode.ThreeDimPlaneEquation(*self.planeParams)
+
+	def testCalcDValueForXyz(self):
+		inpXyz = [1,2,3]
+		expDVal = 3 + 0 + 6
+		actDVal = self.testObjA.calcDForInpXyz(inpXyz)
+		self.assertEqual( expDVal, actDVal )
+
+	def testCalcDistBetweenPlaneAndPoint_planeAlongXy(self):
+		zDist = 4
+		pointCoords = [0.0,0.0,zDist]
+		self.planeParams = [0.0,0.0,1.0,0.0] #This is the xy plane which intercepts z at zero
+		self.createTestObjs()
+		expDistance = zDist
+		actDistance = self.testObjA.getDistanceOfPointFromPlane(pointCoords)
+		self.assertAlmostEqual(expDistance, actDistance)
+
+	def testCalcDistanceBetweenPlaneAndPoint_011_plane(self):
+		pointCoords = [0, 0, 0]
+		self.planeParams = [0.0, 1.0, 1.0, 1.0] #Intercepts both y and z at 1
+		self.createTestObjs()
+		#z=0.5 and y=0.5 should be the closest point; hence the expected distance can be figured out easily
+		expDist = math.sqrt( (0.5**2) + (0.5**2) )
+		actDist = self.testObjA.getDistanceOfPointFromPlane(pointCoords)
+		self.assertAlmostEqual(expDist, actDist)
+
+	def testCalcFromTwoPositionVectors_xyPlane(self):
+		expCoeffs = [0,0,1,0]
+		inpVectors = [ [1,0,0], [0,1,0] ]
+		outObj = tCode.ThreeDimPlaneEquation.fromTwoPositionVectors(*inpVectors, normaliseCoeffs=False)
+		actCoeffs = outObj.coeffs
+
+		for exp,act in it.zip_longest(expCoeffs,actCoeffs):
+			self.assertAlmostEqual(exp,act)
+
+	def testCalcFromTwoPositionVectors_xyPlaneNormalised(self):
+		expCoeffs = [0,0,1,0]
+		inpVectors = [ [2,0,0],[0,3,0] ]
+		outObj = tCode.ThreeDimPlaneEquation.fromTwoPositionVectors(*inpVectors, normaliseCoeffs=True)
+		actCoeffs = outObj.coeffs
+
+		for exp,act in it.zip_longest(expCoeffs,actCoeffs):
+			self.assertAlmostEqual(exp,act)
+
+
+
 
 if __name__ == '__main__':
 	unittest.main()
