5b13fea765c00c1ae732a887c852237bfcb28a79
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-06-10 16:00:00 +0100

    ref_data: Added brucite plane-wave geometry and set of structures for generating eos curves

--- gen_basis_helpers/ref_data/ref_data_brucite.py
+++ gen_basis_helpers/ref_data/ref_data_brucite.py
@@ -1,5 +1,16 @@
 
+import copy
+import os
+
+from ..castep import castep_creator as castepCreator
+from ..shared import config_vars as configVars
+
+
 import plato_pylib.shared.ucell_class as uCell
+
+
+BASE_FOLDER = os.path.join( configVars.CASTEP_DB_PATH, "brucite" )
+
 
 def getExptBruciteStructure():
 	""" Data taken from  https://doi.org/10.1007/BF00202300(Catti 1995, lattice paramters and z-coords) and https://doi.org/10.5006/1322 (remaning fractional co-ordinates) """
@@ -21,6 +32,41 @@ def getExptBruciteStructure():
 	outUCell.convAngToBohr()
 	return outUCell
 	
+
+def getStructsForEos(structType="hexagonal"):
+	if structType!="hexagonal":
+		raise ValueError("Only hexagonal cells implemented at the moment")
+
+	#About 5% in each direction around the experimental value. A bit further for the high volume range though (since PW overestimate volume)
+	aVals = [3.005, 3.029, 3.053, 3.077, 3.102, 3.126, 3.150,
+	         3.174, 3.198, 3.223, 3.247, 3.271, 3.295,
+	         3.319, 3.343, 3.367, 3.391] 
+
+	startCell = getPlaneWaveGeom()
+	cOverA = startCell.lattParams["c"]/startCell.lattParams["a"]
+
+	allOutCells = list()
+	for a in aVals:
+		currLattParams = [a,a,cOverA*a]
+		currCell = copy.deepcopy(startCell)
+		currCell.setLattParams(currLattParams)
+		currCell.convAngToBohr()
+		allOutCells.append(currCell)
+
+	return allOutCells
+
+
+#Plane wave geometry
+def getPlaneWaveGeomParsedFileObject(structType="hexagonal"):
+	structTypeToFunct = {"hexagonal":_getPlaneWaveHexagonalParsedFileObj}
+	return structTypeToFunct[structType.lower()]()
+
+def _getPlaneWaveHexagonalParsedFileObj():
+	refPath = os.path.join(BASE_FOLDER,"opt_geom","hexagonal","geom_opt.castep")
+	return castepCreator.getParsedFileObjFromCastepOutputFile(refPath)
+
+def getPlaneWaveGeom(structType="hexagonal"):
+	return getPlaneWaveGeomParsedFileObject(structType).unitCell
 
 
 
