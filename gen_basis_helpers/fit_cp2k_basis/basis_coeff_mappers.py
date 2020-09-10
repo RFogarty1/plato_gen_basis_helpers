1667a6eed491b1553d6c20951c58455d76a46f52
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-09-10 08:55:43 +0100

    fit_cp2k_basis: Added function for mapping coefficients to a basis function for mixed exponent/coeff optimisation

--- gen_basis_helpers/fit_cp2k_basis/basis_coeff_mappers.py
+++ gen_basis_helpers/fit_cp2k_basis/basis_coeff_mappers.py
@@ -6,6 +6,55 @@ from ..gau_prod_theorem import get_ints_s_expansions as sIntHelp
 
 from . import core
 
+class FitCoeffsToBasisFunctionExponentsAndCoeffsBase():
+	""" Has interface __call__(fitCoeffs)->(exponents,coeffs) where exponents and coeffs are of the same length and lead to the full basis function
+	"""
+
+	def __call__(self, coeffs):
+		raise NotImplementedError("")
+
+
+class FitCoeffsToBasisFunctionExponentsAndCoeffsMixedOptStandard(FitCoeffsToBasisFunctionExponentsAndCoeffsBase):
+	""" Has interface __call__(fitCoeffs)->(exponents,coeffs) where exponents and coeffs are of the same length and lead to the full basis function. NOTE: This implementation requires that exponents MUST be listed first in fitCoeffs. Furthermore coeffs/exponents need to be ordered a certain way (see initializer)
+	"""
+
+	def __init__(self, fixedCoeffs, fixedExponents):
+		""" Initializer
+		
+		Args:
+			IMPORTANT NOTE: The fixedCoeffs/fixedExponents must both be the first n-values in the basis function (if exponents are a1,a2,a3 you cant fix a1 and a3 with this implementation, or even just a3 on its own)
+			fixedCoeffs: (iter of floats) The values of the fixed coefficients. Pass a blank list to vary ALL coefficients
+			fixedExponents: (iter of floats) The values of the fixed exponents. Pass a blank list to vary ALL exponents
+				 
+		"""
+		self.fixedCoeffs = list(fixedCoeffs)
+		self.fixedExponents = list(fixedExponents)
+
+	def __call__(self, fitCoeffs):
+		nTotal = len(fitCoeffs) + len(self.fixedCoeffs) + len(self.fixedExponents)
+		assert nTotal%2==0, "Number of coefficients needs to match number of exponents; not possible with {} total variables".format(nTotal)
+		nEach = int( int(nTotal)/int(2) )
+
+		nFreeExponents = nEach - len(self.fixedExponents)
+		nFreeCoeffs = nEach - len(self.fixedCoeffs)
+
+		outExponents = self.fixedExponents + fitCoeffs[:nFreeExponents]
+		outCoeffs = self.fixedCoeffs + fitCoeffs[nFreeExponents:]
+
+		return outExponents, outCoeffs
+
+class CoeffsToFullBasisFunctionForMixedCoeffExponentOpt(core.CoeffsTransformer):
+
+	def __init__(self, fixedExponents, fixedCoeffs, angMom):
+		self.fixedExponents = fixedExponents
+		self.fixedCoeffs = fixedCoeffs
+		self.angMom = angMom
+
+	def __call__(self, coeffs):
+		mapper = FitCoeffsToBasisFunctionExponentsAndCoeffsMixedOptStandard(self.fixedCoeffs, self.fixedExponents)
+		exponents, outCoeffs = mapper(coeffs)
+		outFunct = parseGau.GauPolyBasis(exponents, [outCoeffs], label=self.angMom)
+		return outFunct
 
 class CoeffsToFullBasisFunctionForFixedExponents(core.CoeffsTransformer):
 	"""Class holds the exponents/angular momentum for a basis function and returns the whole basis function when given the curent coefficients for the the exponents
--- gen_basis_helpers/fit_cp2k_basis/unit_tests/utests_basis_coeff_mappers.py
+++ gen_basis_helpers/fit_cp2k_basis/unit_tests/utests_basis_coeff_mappers.py
@@ -9,6 +9,81 @@ import plato_pylib.plato.parse_gau_files as parseGau
 import gen_basis_helpers.fit_cp2k_basis.basis_coeff_mappers as tCode
 
 
+class TestCoeffsToExponentsAndCoeffsStandard(unittest.TestCase):
+
+	def setUp(self):
+		self.fixedCoeffs = list()
+		self.fixedExponents = [5]
+		self.testCoeffsA = [1,2,3]
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = tCode.FitCoeffsToBasisFunctionExponentsAndCoeffsMixedOptStandard(self.fixedCoeffs, self.fixedExponents)
+
+	def _runFunct(self):
+		return self.testObjA(self.testCoeffsA)
+
+	def testExpectedForNoFixedCoeffs(self):
+		expExponents = self.fixedExponents + [self.testCoeffsA[0]]
+		expCoeffs = self.testCoeffsA[1:]
+		actExponents, actCoeffs  = self._runFunct()
+		self.assertEqual(expCoeffs, actCoeffs)
+		self.assertEqual(expExponents, actExponents)
+
+	def testExpectedForAllFree(self):
+		self.fixedCoeffs, self.fixedExponents = list(), list()
+		expExponents, expCoeffs = [1,2], [3,4]
+		self.testCoeffsA = expExponents + expCoeffs
+		self.createTestObjs()
+		actExponents, actCoeffs = self._runFunct()
+		self.assertEqual(expExponents, actExponents)
+		self.assertEqual(expCoeffs, actCoeffs)
+
+	def testExpecetedForMixOfFixedCoeffsAndExponents(self):
+		self.fixedExponents, self.fixedCoeffs = [1],[3]
+		self.testCoeffsA = [2,4]
+		self.createTestObjs()
+		expExponents, expCoeffs = [1,2], [3,4]
+		actExponents, actCoeffs = self._runFunct()
+		self.assertEqual(expExponents, actExponents)
+		self.assertEqual(expCoeffs, actCoeffs)
+
+	def testExpectedForNoFixedExponents(self):
+		self.fixedExponents, self.fixedCoeffs = list(), [4]
+		self.testCoeffsA = [1,2,3]
+		expExponents, expCoeffs = [1,2], [4,3]
+		self.createTestObjs()
+		actExponents, actCoeffs = self._runFunct()
+		self.assertEqual(expExponents, actExponents)
+		self.assertEqual(expCoeffs, actCoeffs)
+
+	def testRaisesForOddNumberOfCoefficients(self):
+		""" Check we throw an error if we get an odd number of total coefficients """
+		self.testCoeffsA = [1,2]
+		with self.assertRaises(AssertionError):
+			self.assertTrue(False)
+
+
+class TestCoeffsToBasisFunctionMixedOpt(unittest.TestCase):
+
+	def setUp(self):
+		self.fixedExponents = [3]
+		self.fixedCoeffs = [1,2]
+		self.testCoeffsA = [4]
+		self.angMomA = 0
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = tCode.CoeffsToFullBasisFunctionForMixedCoeffExponentOpt(self.fixedExponents, self.fixedCoeffs, self.angMomA)
+
+	@mock.patch("gen_basis_helpers.fit_cp2k_basis.basis_coeff_mappers.parseGau.GauPolyBasis")
+	def testExpectedCallsToGauPoly(self, mockedGauPoly):
+		expObj = mock.Mock()
+		expExponents, expCoeffs = [3,4], [1,2]
+		mockedGauPoly.side_effect = lambda *args,**kwargs: expObj
+		actObj = self.testObjA(self.testCoeffsA)
+		mockedGauPoly.assert_called_with(expExponents, [expCoeffs], label=self.angMomA)
+		self.assertEqual(expObj, actObj)
 
 class TestCoeffsToBasisFunctionForFixedExponents(unittest.TestCase):
 
