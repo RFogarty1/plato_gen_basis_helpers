a63a625f150d632807d66aacb3f6bb8e5064e57a
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-09-22 12:40:09 +0100

    parsed_file_mappers: Added map functions for volume vs total energy

--- gen_basis_helpers/job_helpers/parsed_file_mappers.py
+++ gen_basis_helpers/job_helpers/parsed_file_mappers.py
@@ -21,15 +21,73 @@ class StandardInptXYMapperForParsedFileWorkflows():
 
 class StandardInptMapperForVolumesVsDiagCondNumber(StandardInptXYMapperForParsedFileWorkflows):
 	
-	def __init__(self, divVolsByConstant=None):
-		self.divVolConstant = divVolsByConstant if divVolsByConstant is not None else 1
+	def __init__(self, divVolsByConstant=None, ignoreNoneValues=False):
+		self.divVolByConstant = divVolsByConstant if divVolsByConstant is not None else 1
+		self.ignoreNoneValues = ignoreNoneValues
 	
 	def _getXFunctVals(self, stdInputObj):
-		allGeoms = [x.parsedFile.unitCell for x in stdInputObj.workflow.output]
-		allVolumes = [x.volume/self.divVolConstant for x in allGeoms]
-		return allVolumes
+		return getVolumesFromStdInputObj(stdInputObj, divVolByConstant=self.divVolByConstant, ignoreNoneValues=self.ignoreNoneValues)
 	
 	def _getYFunctVals(self,stdInputObj):
+		return getDiagConditionNumberFromStdInputObj(stdInputObj, ignoreNoneValues=self.ignoreNoneValues)
+
+
+class StandardInptMapperForVolumeVsEnergy(StandardInptXYMapperForParsedFileWorkflows):
+
+	def __init__(self, divVolsByConstant=None, ignoreNoneValues=False):
+		self.divVolByConstant = divVolsByConstant if divVolsByConstant is not None else 1
+		self.ignoreNoneValues = ignoreNoneValues
+
+	def _getXFunctVals(self, stdInputObj):
+		return getVolumesFromStdInputObj(stdInputObj, divVolByConstant=self.divVolByConstant, ignoreNoneValues=self.ignoreNoneValues)
+
+	def _getYFunctVals(self,stdInputObj):
+		return getTotalEnergiesFromStdInputObj(stdInputObj, ignoreNoneValues=self.ignoreNoneValues)
+
+
+#TODO: Factor out the duplication between these functions
+def getVolumesFromStdInputObj(stdInputObj, divVolByConstant=None, ignoreNoneValues=False):
+	if ignoreNoneValues:
+		allGeoms = list()
+		for x in stdInputObj.workflow.output:
+			if x.parsedFile is not None:
+				allGeoms.append(x.parsedFile.unitCell)	
+	else:
+		allGeoms = [x.parsedFile.unitCell for x in stdInputObj.workflow.output]
+
+	allVolumes = [x.volume/divVolByConstant for x in allGeoms]
+	return allVolumes
+
+
+def getDiagConditionNumberFromStdInputObj(stdInputObj, ignoreNoneValues=False):
+	if ignoreNoneValues:
+		allVals = list()
+		for x in stdInputObj.workflow.output:
+			if x.parsedFile is not None:
+				allVals.append(x.parsedFile.overlap_condition_number.diag.twoNorm)
+	else:
 		allVals = [x.parsedFile.overlap_condition_number.diag.twoNorm for x in stdInputObj.workflow.output]
-		return allVals
+
+	return allVals
+
+
+def getTotalEnergiesFromStdInputObj(stdInputObj, ignoreNoneValues=False):
+	if ignoreNoneValues:
+		allVals = list()
+		for x in stdInputObj.workflow.output:
+			if x.parsedFile is not None:
+				allVals.append(x.parsedFile.energies.electronicTotalE)
+	else:
+		allVals = [x.parsedFile.energies.electronicTotalE for x in stdInputObj.workflow.output]
+
+	return allVals
+
+
+
+
+
+
+
+
+
 
