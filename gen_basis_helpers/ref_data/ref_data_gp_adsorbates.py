c9333791c8f6171ffd766260e43c08d51f1c359a
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-10-05 09:26:28 +0100

    gp_adsorbates: Added castep versions

--- gen_basis_helpers/ref_data/ref_data_gp_adsorbates.py
+++ gen_basis_helpers/ref_data/ref_data_gp_adsorbates.py
@@ -5,6 +5,8 @@ import os
 
 import plato_pylib.parseOther.parse_qe_files as parseQE
 
+from . import helpers_ref_data as refDataHelpers
+
 from ..shared import config_vars as configVars
 from ..shared import register_key_decorator as regKeyDeco
 
@@ -19,7 +21,7 @@ _SINGLE_ADSORBATE_ENERGY_DICT = dict()
 registerGeomDeco = regKeyDeco.RegisterKeyValDecorator(_SINGLE_ADSORBATE_GEOM_DICT, forceKeysToCase="lower")
 registerTotalEnergyDeco = regKeyDeco.RegisterKeyValDecorator(_SINGLE_ADSORBATE_ENERGY_DICT, forceKeysToCase="lower") 
 
-#These come from quantum espresso
+
 def getPlaneWaveGasPhaseAdsorbateGeom(adsorbate):
 	""" Get plane-wave optimised geometry for a gas phase molecule/atom
 	
@@ -47,34 +49,58 @@ def getPlaneWaveGasPhaseAdsorbateEnergy(adsorbate):
 	return _SINGLE_ADSORBATE_ENERGY_DICT[adsorbate.lower()]()
 
 
+@registerGeomDeco("h2_castep")
 @registerGeomDeco("h2")
+def _getH2GeomCastep():
+	filePath = os.path.join(BASE_FOLDER, "h2", "castep", "geom_opt.castep")
+	return refDataHelpers.getUCellInBohrFromCastepOutFile(filePath)
+
+@registerTotalEnergyDeco("h2_castep")
+@registerTotalEnergyDeco("h2")
+def _getH2EnergyCastep():
+	filePath = os.path.join(BASE_FOLDER, "h2", "castep", "geom_opt.castep")
+	return refDataHelpers.getEnergyFromCastepOutFile(filePath)
+
+@registerGeomDeco("h2_espresso")
 def _getH2Geom():
 	filePath = os.path.join(BASE_FOLDER,"h2","hydrogen.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["unitCell"] 
 
-@registerTotalEnergyDeco("h2")
+@registerTotalEnergyDeco("h2_espresso")
 def _getH2Energy():
 	filePath = os.path.join(BASE_FOLDER,"h2","hydrogen.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["energies"].electronicTotalE 
 
 
-@registerGeomDeco("hydroxyl")
+@registerGeomDeco("hydroxyl_espresso")
 def _():
 	filePath = os.path.join(BASE_FOLDER,"hydroxyl", "hydroxyl.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["unitCell"] 
 
-@registerTotalEnergyDeco("hydroxyl")
+@registerTotalEnergyDeco("hydroxyl_espresso")
 def _():
 	filePath = os.path.join(BASE_FOLDER,"hydroxyl", "hydroxyl.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["energies"].electronicTotalE 
 
 
+@registerGeomDeco("h2o_castep")
 @registerGeomDeco("h2o")
+def _():
+	filePath = os.path.join(BASE_FOLDER,"h2o","castep", "geom_opt.castep")
+	return refDataHelpers.getUCellInBohrFromCastepOutFile(filePath)
+
+@registerTotalEnergyDeco("h2o")
+@registerTotalEnergyDeco("h2o_castep")
+def _():
+	filePath = os.path.join(BASE_FOLDER,"h2o","castep", "geom_opt.castep")
+	return refDataHelpers.getEnergyFromCastepOutFile(filePath)
+
+@registerGeomDeco("h2o_espresso")
 def _():
 	filePath = os.path.join(BASE_FOLDER,"h2o", "water.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["unitCell"] 
 
-@registerTotalEnergyDeco("h2o")
+@registerTotalEnergyDeco("h2o_espresso")
 def _():
 	filePath = os.path.join(BASE_FOLDER,"h2o", "water.out")
 	return parseQE.parseQuantumEspressoOutfile(filePath)["energies"].electronicTotalE 
