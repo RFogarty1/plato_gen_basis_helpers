3048b23eb88af9da7eeaa7b12f003c548fc63ef9
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-10-29 11:57:41 +0000

    refactor: Moved GetDictFromCreatorObj to shared (from CP2K)
    Didnt really require any changes

--- gen_basis_helpers/cp2k/cp2k_creator_to_dict.py
+++ gen_basis_helpers/cp2k/cp2k_creator_to_dict.py
@@ -1,44 +1,13 @@
 
+from ..shared import method_objs as methObjsHelp
 
 def getSimpleCreatorObjToDictMapObj():
 	outFuncts = list()
 	outFuncts.append(getSelectedBasicInfoDictFromCreatorObj)
 	outFuncts.append(_getSelectedDictsFromCreatorObj)
 	outFuncts.append(_getGeomConstraintInfoFromCreatorObj)
-	return GetDictFromCreatorObj(outFuncts)
+	return methObjsHelp.GetDictFromCreatorObj(outFuncts)
 
-class GetDictFromCreatorObj():
-	""" Callable class for getting a dictionary object from a BaseCP2KCalcObjFactory object. Callable interface is f(BaseCP2KCalcObjFactory)->dict
-
-	"""
-
-	def __init__(self, creatorToDictFuncts, modFinalDictFuncts=None):
-		""" Initialiser
-		
-		Args:
-			creatorToDictFuncts: (iter of functions) Each function takes a BaseCP2KCalcObjFactory instance and returns a dictionary
-			modFinalDictFuncts: (iter of functions, Optional) Each function modifies a dictionary in place
-
-		Important notes (for args):
-			The creatorToDictFuncts are each called first IN ORDER. The output dict updates with EACH of the dicts produced by these functions, so if two return the same key then the second will overwrite the first. The modFinalDictFuncts are run AFTER the creatorToDictFuncts, so they can do things lke remove certain keys if required.
-				 
-		"""
-		self.creatorToDictFuncts = list(creatorToDictFuncts)
-		self.modFinalDicts = list() if modFinalDictFuncts is None else list(modFinalDictFuncts)
-
-
-	def _getOutputDict(self, creator):
-		outDict = dict()
-		for x in self.creatorToDictFuncts:
-			outDict.update( x(creator) )
-
-		for x in self.modFinalDicts:
-			x(outDict)
-
-		return outDict
-
-	def __call__(self, creator):
-		return self._getOutputDict(creator)
 
 def getSelectedBasicInfoDictFromCreatorObj(creatorObj):
 	outDict = dict()
--- gen_basis_helpers/cp2k/unit_tests/utests_creator_to_dict.py
+++ gen_basis_helpers/cp2k/unit_tests/utests_creator_to_dict.py
@@ -26,36 +26,4 @@ class TestGetDictFromCreatorFuncts(unittest.TestCase):
 		actDict = tCode.getSelectedBasicInfoDictFromCreatorObj(self.creatorObjA)
 		self.assertEqual(expDict,actDict)
 
-class TestGetDictFromObjCreatorClass(unittest.TestCase):
-
-	def setUp(self):
-		self.retDictA = {"key_a":"val_a" , "key_b":"val_b"}
-		self.retDictB = {"key_a":"val_a2", "key_c":"val_c"}
-		self.mockCreatorA = mock.Mock()
-		self.createTestObjs()
-
-	def createTestObjs(self):
-		self.getterFunctA = mock.Mock()
-		self.getterFunctB = mock.Mock()
-		self.getterFunctA.side_effect = lambda x: self.retDictA 
-		self.getterFunctB.side_effect = lambda x: self.retDictB
-		self.delFunctA = lambda x: x.pop("key_a")
-
-	def testWithSingleGetterFunct(self):
-		testObj = tCode.GetDictFromCreatorObj([self.getterFunctA])
-		expOutput = self.retDictA
-		actOutput = testObj( self.mockCreatorA )
-		self.assertEqual(expOutput, actOutput)
-
-	def testWithTwoGetterFuncts_secondOverwriting(self):
-		testObj = tCode.GetDictFromCreatorObj([self.getterFunctA, self.getterFunctB])
-		expOutput = {"key_a":"val_a2", "key_b":"val_b", "key_c":"val_c"}
-		actOutput = testObj( self.mockCreatorA )
-		self.assertEqual(expOutput, actOutput)
-
-	def testWithFinalFunctDeletingKeys(self):
-		testObj = tCode.GetDictFromCreatorObj([self.getterFunctA], modFinalDictFuncts=[self.delFunctA])
-		expOutput = {"key_b":"val_b"}
-		actOutput = testObj( self.mockCreatorA )
-		self.assertEqual(expOutput, actOutput)
 
