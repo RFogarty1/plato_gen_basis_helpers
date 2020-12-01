82734049118d1de5de5cbc11e7fd25227d051cbe
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-12-01 11:32:23 +0000

    special_builders: Added code to build bulk water regions from surface+adsorbates geoms

--- gen_basis_helpers/special_builders/unit_tests/utests_water_box.py
+++ gen_basis_helpers/special_builders/unit_tests/utests_water_box.py
@@ -1,9 +1,15 @@
 
+import copy
+import types
 import unittest
 import unittest.mock as mock
 
+import plato_pylib.utils.supercell as supCellHelp
 import plato_pylib.shared.ucell_class as uCellHelp
 
+
+import gen_basis_helpers.adsorption.adsorbate_detectors as adsDetectHelp
+import gen_basis_helpers.adsorption.surface_detectors as surfDetectHelp
 import gen_basis_helpers.special_builders.water_box as tCode
 
 
@@ -32,6 +38,156 @@ class TestGetLatticeParameterForTargetDensity(unittest.TestCase):
 		expVal = 0.1
 		actVal = self.runTestFunct()
 		self.assertAlmostEqual(expVal, actVal)
+
+
+class TestGetBulkWaterEmptyBoxForSurfWithAdsBothSides(unittest.TestCase):
+
+	def setUp(self):
+		self.lattParamsA = [10,10,10]
+		self.lattAnglesA = [90,90,90]
+		self.adsSymbol = "ads"
+		self.surfAtomSymbol = "surf"
+		self.cartCoordsA = [ [0,0,8,self.adsSymbol],
+		                     [0,0,7,self.adsSymbol],
+		                     [0,0,6,self.surfAtomSymbol],
+		                     [0,0,5,self.surfAtomSymbol],
+		                     [0,0,4,self.adsSymbol],
+		                     [0,0,3,self.adsSymbol] ]
+
+		self.densityA = mock.Mock()
+		self.massDict = None
+		self.extraHeight = 0
+		self.targDensity = 2
+		self.nWaterTotal = 30
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testCellA = uCellHelp.UnitCell(lattParams=self.lattParamsA, lattAngles=self.lattAnglesA)
+		self.testCellA.cartCoords = self.cartCoordsA
+		self._createAdsorbateDetectors()
+		currArgs = [self.adsDetectorTopA, self.adsDetectorBotA, self.densityA]
+		self.testObjA = tCode.GetBulkWaterEmptyBoxFromSurfWithAdsorbatesBothSidesStandard(*currArgs)
+
+	def _createAdsorbateDetectors(self):
+		surfDetector = surfDetectHelp.DetectSurfaceBasedOnElementsPresent([self.surfAtomSymbol])
+		topFilterFunct = adsDetectHelp.FilterToAtomsAboveOrBelowSurf(surfDetector, top=True)
+		botFilterFunct = adsDetectHelp.FilterToAtomsAboveOrBelowSurf(surfDetector, top=False)
+		self.adsDetectorTopA = adsDetectHelp.DetectSimpleAtomicAdsorbateFromInpGeom(self.adsSymbol,postFilterFuncts=[topFilterFunct])
+		self.adsDetectorBotA = adsDetectHelp.DetectSimpleAtomicAdsorbateFromInpGeom(self.adsSymbol,postFilterFuncts=[botFilterFunct])
+
+	@mock.patch("gen_basis_helpers.special_builders.water_box.findLatticeParameterToGetTargetDensityForNWater")
+	def testForSimpleCellA(self, mockedGetLatticeParam):
+		mockHeightTotal = 20
+		mockedGetLatticeParam.side_effect = lambda *args,**kwargs: mockHeightTotal
+		expNWaterTotal = self.nWaterTotal - 4 #4 is the number of self.adsSymbol
+		totalHeightAdsLayers = 1+1 #Theres a gap of 1 between ads above and below the cell
+		expOutCell = uCellHelp.UnitCell(lattParams=[10,10,mockHeightTotal-totalHeightAdsLayers], lattAngles=[90,90,90])
+		actOutCell,unused = self.testObjA(self.testCellA, self.nWaterTotal, targDensity=self.targDensity)
+		mockedGetLatticeParam.assert_called_with(self.testCellA, expNWaterTotal, self.targDensity, lattParam="c", massDict=self.massDict)
+		expOutCell.fractCoords = list()
+		self.assertEqual(expOutCell, actOutCell)
+
+	@mock.patch("gen_basis_helpers.special_builders.water_box.findLatticeParameterToGetTargetDensityForNWater")
+	def testForSimpleCellA_plusFourHeight(self, mockedGetLatticeParam):
+		mockHeightTotal = 20
+		mockedGetLatticeParam.side_effect = lambda *args,**kwargs:mockHeightTotal
+		totalHeightAdsLayers = 1+1
+		extraHeight = 4
+		expOutCell = uCellHelp.UnitCell(lattParams=[10,10,mockHeightTotal-totalHeightAdsLayers+extraHeight], lattAngles=[90,90,90])
+		actOutCell,unused = self.testObjA(self.testCellA, self.nWaterTotal, targDensity=self.targDensity,extraHeight=extraHeight)
+		expOutCell.fractCoords = list()
+		self.assertEqual(expOutCell, actOutCell)		
+
+	@mock.patch("gen_basis_helpers.special_builders.water_box.findLatticeParameterToGetTargetDensityForNWater")
+	def testExpectedNumberOfWaterReturned(self, mockedGetLatticeParam):
+		mockHeightTotal = 20
+		mockedGetLatticeParam.side_effect = lambda *args,**kwargs:mockHeightTotal
+		expNumberWater = self.nWaterTotal - 4
+		unused, actNumberWater = self.testObjA(self.testCellA, self.nWaterTotal, targDensity=self.targDensity)
+		self.assertEqual(expNumberWater,actNumberWater)
+
+
+
+class TestGetWaterBoxForMD(unittest.TestCase):
+
+	def setUp(self):
+		self.adsObjA = types.SimpleNamespace( geom=[ [0,0,0,"ads"] ] )
+		self.lattParamsA, self.lattAnglesA = [5,5,5], [90,90,90]
+		self.cartCoordsA = [[3,3,3,"X"]]
+		self.waterAdsGapA = 0
+		self.nWater = 2
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.singleCellA = uCellHelp.UnitCell(lattParams=self.lattParamsA, lattAngles=self.lattAnglesA)
+		self.singleCellA.cartCoords = self.cartCoordsA
+		self.adsObjsA = [self.adsObjA]
+		self.testObjA = tCode.GetWaterBoxForMDFromEmptyBoxStandard(self.singleCellA, self.adsObjsA,waterAdsGap=self.waterAdsGapA)
+		self.emptyCellA = supCellHelp.superCellFromUCell(self.singleCellA,[2,1,1])
+		self.emptyCellA.cartCoords = list()
+
+	def testExpectedForSimpleOneLatticeSiteCell(self):
+		expCartCoords = [ [3,3,3,"ads"], [8,3,3,"ads"] ]
+		expCell = copy.deepcopy(self.emptyCellA)
+		expCell.cartCoords = expCartCoords
+		actCell = self.testObjA(self.emptyCellA, self.nWater)
+		self.assertEqual(expCell,actCell)
+
+	@mock.patch("gen_basis_helpers.special_builders.water_box.boxFillHelp.CellFillerStandard")
+	def testExpectedCallsForSimpleWithAdsGap(self, mockedCellFiller):
+		self.waterAdsGapA = 1
+		self.createTestObjs()
+		mockFillObj = mock.Mock()
+		mockedCellFiller.side_effect = lambda *args, **kwargs: mockFillObj
+		mockFillObj.side_effect = lambda *args,**kwargs: list()
+		expCellToFill = uCellHelp.UnitCell( lattParams=[10,5,3],lattAngles=[90,90,90] )
+		expCellToFill.cartCoords = list()
+		self.testObjA(self.emptyCellA, self.nWater)
+		mockFillObj.assert_called_with(self.nWater, expCellToFill)
+
+	def testExpectedLattParamsWhenAdsGapDefined(self):
+		self.waterAdsGapA = 0.1
+		self.createTestObjs()
+		expCell = uCellHelp.UnitCell(lattParams=[10,5,5-(2*self.waterAdsGapA)],lattAngles=[90,90,90])
+		actCell = self.testObjA(self.emptyCellA,self.nWater)
+		expCell.cartCoords = list()
+		actCell.cartCoords = list()
+		self.assertEqual(expCell,actCell)
+
+
+
+class testGetBulkWaterFromSurfWithAdsorbed(unittest.TestCase):
+
+	def setUp(self):
+		self.mockInputCell = mock.Mock()
+		self.expEmptyBox = mock.Mock()
+		self.expBoxForMD = mock.Mock()
+		self.expWaterAdsGap = 4
+		self.nWaterTotal = 4
+		self.nWaterForBox = self.nWaterTotal-2
+		self.targDensity, self.extraHeight = 5, 7
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.mockGetEmptyBox = mock.Mock()
+		self.mockFillEmptyBox = mock.Mock()
+		self.mockGetEmptyBox.side_effect = lambda *args, **kwargs: (self.expEmptyBox, self.nWaterForBox)
+		self.mockFillEmptyBox.side_effect = lambda *args, **kwargs: self.expBoxForMD
+		self.mockFillEmptyBox.waterAdsGap = self.expWaterAdsGap
+		currArgs = [self.mockGetEmptyBox, self.mockFillEmptyBox]
+		self.testObjA = tCode.GetBulkWaterBoxesFromSurfWithAdsorbatesBothSidesStandard(*currArgs)
+
+
+	def testExpectedOutputWithEverythingMocked(self):
+		currKwargs = {"cellForMD":self.expBoxForMD, "emptyCell":self.expEmptyBox, "waterAdsGap":self.expWaterAdsGap}
+		expObj = types.SimpleNamespace( **currKwargs )
+		actObj = self.testObjA(self.mockInputCell, self.nWaterTotal,targDensity=self.targDensity,
+		                       extraHeight=self.extraHeight, waterAdsGap=self.expWaterAdsGap)
+
+		self.mockGetEmptyBox.assert_called_with(self.mockInputCell, self.nWaterTotal, targDensity=self.targDensity, extraHeight=self.extraHeight)
+		self.mockFillEmptyBox.assert_called_with(self.expEmptyBox, self.nWaterForBox, waterAdsGap=self.expWaterAdsGap)
+		self.assertEqual(expObj,actObj)
+
 
 
 
--- gen_basis_helpers/special_builders/water_box.py
+++ gen_basis_helpers/special_builders/water_box.py
@@ -1,10 +1,193 @@
 
+import copy
 import itertools as it
+import types
 
 import plato_pylib.shared.ucell_class as uCellHelp
 import plato_pylib.shared.unit_convs as uConvHelp
 
 from ..adsorption import water_adsorbate as waterAdsHelp
+from ..shared import cart_coord_utils as cartHelp
+
+from . import fill_box as boxFillHelp
+
+
+class GetBulkWaterBoxesFromSurfWithAdsorbatesBothSidesStandard():
+	""" Callable class for getting boxes of bulk water relevant to MD for a given surface+adsorbates(both sides) geometry
+	"""
+	def __init__(self, getEmptyBox, fillEmptyBox):
+		""" Initializer
+		
+		Args:
+			getEmptyBox: (GetBulkWaterEmptyBoxFromSurfWithAdsorbatesBothSidesStandard object)
+			fillEmptyBox: (GetWaterBoxForMDFromEmptyBoxStandard object)
+ 
+		"""
+		self.getEmptyBox = getEmptyBox
+		self.fillEmptyBox = fillEmptyBox
+
+
+	def getBulkWaterBoxes(self, inpCell, nWater, targDensity=None, extraHeight=None, waterAdsGap=None):
+		""" Gets boxes relevant to simulating the bulk water region for a surface with water adsorbed on both sides
+		
+		Args:
+			inpCell: (plato_pylib UnitCell object) Cell containing surface geometry with adsorbates on both sides
+			nWater: (int) Total number of water molecules required for the simulation
+
+		Kwargs:
+			These are all attributes of either self.getEmptyBox or self.fillEmptyBox. Thus, will default to those values if not set. See the relevant docstrings on self.getEmptyBox and self.fillEmptyBox for details
+ 
+		Returns
+			outCells: (SimpleNamespace) Contains multiple cells explained below
+	 
+		outCells:
+			cellForMD: (plato_pylib UnitCell) This is the cell that should be used to get starting co-ordinates for the bulk water region
+			emptyCell: (plato_pylib UnitCell) This is the cell without any water present AND without a water-Ads gap taken into account. Therefore it may be sligtly larger than cellForMD
+			waterAdsGap: (float) This is the gap enforced between the bulk water region and the adsorbate region. Its enforced by making cellForMD smaller than needed based on the density target
+
+		"""
+		emptyBox, nWaterBulk = self.getEmptyBox(inpCell, nWater, targDensity=targDensity, extraHeight=extraHeight)
+		cellForMD = self.fillEmptyBox(emptyBox, nWaterBulk, waterAdsGap=waterAdsGap)
+		outAdsGap = self.fillEmptyBox.waterAdsGap if waterAdsGap is None else waterAdsGap
+		outDict = {"emptyCell":emptyBox, "cellForMD":cellForMD, "waterAdsGap":outAdsGap}
+		return types.SimpleNamespace(**outDict)
+
+	def __call__(self, inpCell, nWater, targDensity=None, extraHeight=None, waterAdsGap=None):
+		return self.getBulkWaterBoxes(inpCell, nWater, targDensity=targDensity, extraHeight=extraHeight, waterAdsGap=waterAdsGap)
+
+
+class GetWaterBoxForMDFromEmptyBoxStandard():
+	""" Callable class for getting a unitCell filled with water from an empty cell and number of water molecules, see getMDCell for interface
+
+	"""
+
+	def __init__(self, primCell, waterAdsObjs, waterAdsGap=0):
+		""" Initializer
+		
+		Args:
+			waterAdsGap: (float) Gap between an adsorbate layer and bulk water. This gets subtraced from BOTH ends of the cell (so set to 0.5*excludeDist where excludeDist is the total amount you want the cell shortened by)
+			primCell: (plato_pylib UnitCell object) This represents the smallest unit cell of the bulk system with each co-ordinate representing a lattice site (symbol is irrelevant). 
+			waterAdsObjs: (list of Adsorbate objects) The length of this needs to match the length of singleHexCell
+
+		"""
+		self.primCell = primCell
+		self.waterAdsObjs = waterAdsObjs
+		self.waterAdsGap = waterAdsGap
+
+
+	def getMDCell(self, emptyCell, numbWater, waterAdsGap=None):
+		cellFiller = boxFillHelp.CellFillerStandard(self.primCell)
+		outCell = self._getEmptyCellToUseFromInpCell(emptyCell, waterAdsGap=waterAdsGap)
+		lattSites = cellFiller(numbWater, outCell)
+		outCoords = self._getCartCoordsForAdsorbatesAtLattSites(lattSites)
+		outCell.cartCoords = outCoords
+		return outCell
+
+	def _getCartCoordsForAdsorbatesAtLattSites(self, lattSiteCoords):
+		adsObjs = it.cycle(self.waterAdsObjs)
+		outCoords = list()
+		for currSite,adsObj in zip(lattSiteCoords,adsObjs):
+			for coord in adsObj.geom:
+				currXYZ = [x+t for x,t in zip(coord,currSite[:3])]
+				currXYZ.append(coord[-1])
+				outCoords.append(currXYZ)
+		return outCoords
+
+	def _getEmptyCellToUseFromInpCell(self, inpCell, waterAdsGap=None):
+		waterAdsGap = self.waterAdsGap if waterAdsGap is None else waterAdsGap
+		outLattParams = [x for x in inpCell.getLattParamsList()]
+		outLattParams[-1] -= self.waterAdsGap*2
+		outLattAngles = inpCell.getLattAnglesList()
+		outCell = uCellHelp.UnitCell(lattParams=outLattParams,lattAngles=outLattAngles)
+		return outCell
+
+	def __call__(self, emptyCell, numbWater, waterAdsGap=None):
+		return self.getMDCell(emptyCell, numbWater, waterAdsGap=waterAdsGap)
+
+
+
+class GetBulkWaterEmptyBoxFromSurfWithAdsorbatesBothSidesStandard():
+	""" Callable class (see self.getBoxAndNumberOfWaterToAdd for interface) for getting an empty box for a surface with water on both sides. The box height is set such that adding (N_tot-N_ads) water molecules to it will produce the correct density between the surface layers
+
+		WARNING: This wont work if your adsorbates cross periodic boundaries in the surface plane.
+
+	"""
+
+	def __init__(self,adsDetectorTop, adsDetectorBot, targDensity, extraHeight=0, massDict=None):
+		""" Initializer
+		
+		Args:
+			adsDetectorTop: (AdsorbatesFromInpGeom object) Used to get adsorbate objects above the surface from an input UnitCell object
+			adsDetectorBot: (AdsorbatesFromInpGeom object) Used to get adsorbate objects below the surface from an input UnitCell object
+			targDensity: (float) Target density for the combined bulk and adsorbed water region. This region is h_adsA, h_adsB + h_bulk, where h_adsA/B are interplane spacings between the top/bottom of each adsorbate layer.
+			extraHeight: (float, Optional) Value added to the water-region height. Generally passed at runtime(rather than initiation) and used to generate a series of cells with slightly different density values (the stress-tensor for runs on these may then be calculated to find which best represents the NVT ensemble at a given pressure)
+			massDict: (dict, Optional) The dictionary used to translate element symbols to masses. Default will likely always be fine though
+ 
+		"""
+		self.adsDetectorTop = adsDetectorTop
+		self.adsDetectorBot = adsDetectorBot
+		self.targDensity = targDensity
+		self.extraHeight = extraHeight
+		self.massDict = massDict
+
+	#I MAY add another function that returns both the empty box AND the number of water molecules that need adding to it
+	def getBox(self, inpCell, nWaterTotal, targDensity=None, extraHeight=None):
+		""" Get a UnitCell object for the bulk water region of an MD simulation
+		
+		Args:
+			inpCell: (UnitCell object) Geometry of the surface with adsorbates.
+			nWaterTotal: (int) The total number of water molecules you want in your surface+water simulation. This must be at least as many water molecules as found in inpCell
+			extraHeight: (float, Optional) Value added to the water-region height. Default is that passed by initialiation of this instance
+			targDensity: (float,Optional): Target density in g/units**3 where units are the length units used in inpCell. Default value is determined on initliations; also see the __init__ docstring for a more precise meaning of density
+ 
+		Returns
+			emptyBox: (UnitCell object) The cell used to contain the bulk water
+	 
+		"""
+		return self.getBoxAndNumberOfWaterToAdd(inpCell,nWaterTotal, targDensity=targDensity, extraHeight=extraHeight)[0]
+
+	def getBoxAndNumberOfWaterToAdd(self, inpCell, nWaterTotal, targDensity=None, extraHeight=None):
+		""" See getBox for details. Only difference is that this function also returns the number of water molecules to add to the box as the second returned parameter
+		"""
+		targDensity = self.targDensity if targDensity is None else targDensity
+		extraHeight = self.extraHeight if extraHeight is None else extraHeight
+
+		#Get all adsorbates above/below
+		aboveAdsorbates = self.adsDetectorTop(inpCell)
+		belowAdsorbates = self.adsDetectorBot(inpCell)
+		heightTopAds = _getHeightOfAdsorbateLayer(aboveAdsorbates, inpCell)
+		heightBotAds = _getHeightOfAdsorbateLayer(belowAdsorbates, inpCell)
+		numbWaterInCell = len(aboveAdsorbates) + len(belowAdsorbates)
+		nBulkWater = nWaterTotal - numbWaterInCell
+
+		#Get the length of the cell for bulk geom
+		cellLen = findLatticeParameterToGetTargetDensityForNWater(inpCell, nBulkWater, targDensity, lattParam="c", massDict=self.massDict)
+		cellLen -= (heightTopAds + heightBotAds)
+		cellLen += extraHeight
+		outCell = copy.deepcopy(inpCell)
+		lattParams = outCell.getLattParamsList()
+		lattParams[-1] = cellLen
+		outCell.setLattParams(lattParams)
+		outCell.fractCoords = list()
+
+		return outCell,nBulkWater
+
+
+	def __call__(self, inpCell, nWater, targDensity=None, extraHeight=None):
+		return self.getBoxAndNumberOfWaterToAdd(inpCell, nWater, targDensity=targDensity, extraHeight=extraHeight)
+
+
+#Wont work when they cross PBCs
+def _getHeightOfAdsorbateLayer(adsorbateObjs, inpCell):
+	#Get all signed distances from a random reference point
+	topPlaneEqn = cartHelp.getPlaneEqnForOuterSurfaceAtoms(inpCell)
+	outSignedDists = list()
+	for adsObj in adsorbateObjs:
+		for atomGeom in adsObj.geom:
+			currSignedDist = topPlaneEqn.getSignedDistanceOfPointFromPlane(atomGeom[:3])
+			outSignedDists.append(currSignedDist)
+
+	return max(outSignedDists)-min(outSignedDists)
 
 
 def findLatticeParameterToGetTargetDensityForNWater(inpCell, nWater, targDensity, lattParam="c", massDict=None):
