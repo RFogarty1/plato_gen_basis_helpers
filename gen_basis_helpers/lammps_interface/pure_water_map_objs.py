47a5feb336fc8b7d99db7c262281f00ac14a6f76
Richard M Fogarty <richard.m.fogarty@gmail.com>
2020-12-02 12:15:49 +0000

    lammps_interface: Added callable class for mapping pure-water unitCell geom to lammps geom obj

--- gen_basis_helpers/lammps_interface/pure_water_map_objs.py
+++ gen_basis_helpers/lammps_interface/pure_water_map_objs.py
@@ -1,4 +1,67 @@
 
+import copy
+from . import lammps_geom as lammpsGeomHelp
+
+class PureWaterCellToLammpsGeomMapperCentralOnly():
+	""" Callable class (see getLammpsGeom for interface) to get a LAMMPS geometry object from a UnitCell object containing a pure-water system. NOTE: Won't work if any water molecules cross periodic boundaries
+
+	"""
+	def __init__(self, waterDetector, eleToTypeIdx, eleToCharge=None, eleToMass=None, convBohrToAng=True, modTiltFactors=None, atomStyle="full"):
+		""" Initializer
+		
+		Args:
+			waterDetector: (DetectH2OAdsorbatesFromInpGeomStandard object). This has a .getCentralCentralAdsorbateIndices function. NOTE: This needs setting up such that it works for geometries in ANGSTROMS
+			eleToTypeIdx: (Dict) Keys are element strs while values are the type indices for this element
+			eleToCharge: (Dict) Keys are element strs while values are the charge to associate with it
+			eleToMass: (Dict) Keys are element strs while values are the mass to associate with it. Default value almost always will be fine
+			convBohrToAng: (Bool) If True then the output cell will be converted to angstrom (from bohr)
+			modTiltFactors: (list) Factors applied to tilt factors for lammps. This is mainly for hexgonal cells, where tilt factors include 0.5, which can become slightly greater than 0.5 (and throw an error in lammps) due to floats imprecision (or similar). Thus recommended to set to [0.999,0.999,0.999] in this situation. Effectively defaults to [1,1,1]. 
+			atomStyle:(str) Only "full" currently supported, so just dont change from default
+ 
+		"""
+		self.waterDetector = waterDetector
+		self.eleToTypeIdx = eleToTypeIdx
+		self.eleToCharge = eleToCharge
+		self.eleToMass = eleToMass
+		self.convBohrToAng = convBohrToAng
+		self.modTiltFactors = modTiltFactors
+		self.atomStyle = atomStyle
+
+	def getLammpsGeom(self, inpGeom):
+		""" Get a LammpsGeom object from a plato_pylib UnitCell
+		
+		Args:
+			inpCell: (plato_pylib UnitCell object) Must only have water present, and water bonds should not cross PBCs (MIGHT be ok if some atoms are outside the cell boundaries, definitely will not work )
+				 
+		Returns:
+			outObj: (LammpsGeom) Lammps geom object for input geometry
+	 
+		"""
+		if self.atomStyle=="full":
+			getDataDictFunct = lammpsGeomHelp.GetDataDictFromLammpsGeomAtomStyleFull(modTiltFactors=self.modTiltFactors)
+		else:
+			raise ValueError("{} is an invalid atomStyle".format(self.atomStyle))
+
+		#Create the map functions
+		geomToBondInfo  = GetPureWaterBondsFromInpGeomCentralCellOnly(self.waterDetector)
+		geomToAngleInfo = GetPureWaterAnglesFromInpGeomCentralCellOnly(self.waterDetector)
+		geomToMoleculeIDs = GetPureWaterMoleculeIndicesFromInpGeomCentralCellOnly(self.waterDetector)
+
+		#Modify the units on inpCell if requested
+		outGeom = copy.deepcopy(inpGeom)
+		if self.convBohrToAng:
+			outGeom.convBohrToAng()
+
+		#Create the output object
+		outKwargs = {"eleToTypeIdx":self.eleToTypeIdx, "eleToCharge":self.eleToCharge, "eleToMass":self.eleToMass,
+		             "geomToBondInfo":geomToBondInfo, "geomToAngleInfo":geomToAngleInfo, "geomToMoleculeIDs":geomToMoleculeIDs,
+		             "getDataDictFunct":getDataDictFunct}
+		outObj = lammpsGeomHelp.LammpsGeom(outGeom, **outKwargs)
+		return outObj
+
+	def __call__(self, inpGeom):
+		return self.getLammpsGeom(inpGeom)
+
 
 class GetPureWaterBondsFromInpGeomCentralCellOnly():
 
@@ -24,6 +87,13 @@ class GetPureWaterBondsFromInpGeomCentralCellOnly():
 				currIdx += 1
 
 		return outIndices
+
+	def __eq__(self, other):
+		if self.bondTypeIdx != other.bondTypeIdx:
+			return False
+		if self.adsDetector != other.adsDetector:
+			return False
+		return True
 
 	def __call__(self, inpGeom):
 		return self.getBondsFromInpGeom(inpGeom)
@@ -52,6 +122,13 @@ class GetPureWaterAnglesFromInpGeomCentralCellOnly():
 			currIdx+=1
 		return outIndices
 
+	def __eq__(self, other):
+		if self.angleTypeIdx != other.angleTypeIdx:
+			return False
+		if self.adsDetector != other.adsDetector:
+			return False
+		return True
+
 	def __call__(self, inpGeom):
 		return self.getAnglesFromInpGeom(inpGeom)
 
@@ -78,6 +155,11 @@ class GetPureWaterMoleculeIndicesFromInpGeomCentralCellOnly():
 				outIndices[atomIdx] = molIdx
 
 		return outIndices
+
+	def __eq__(self, other):
+		if self.adsDetector != other.adsDetector:
+			return False
+		return True
 	
 	def __call__(self, inpGeom):
 		return self.getMoleculeIndicesFromGeom(inpGeom)
--- gen_basis_helpers/lammps_interface/unit_tests/utests_pure_water_map_objs.py
+++ gen_basis_helpers/lammps_interface/unit_tests/utests_pure_water_map_objs.py
@@ -1,5 +1,5 @@
 
-
+import copy
 import unittest
 import unittest.mock as mock
 
@@ -8,6 +8,52 @@ import plato_pylib.shared.ucell_class as uCellHelp
 
 import gen_basis_helpers.adsorption.adsorbate_detectors as adsDetectHelp
 import gen_basis_helpers.lammps_interface.pure_water_map_objs as tCode
+
+
+class TestGetLammpsGeomFromInpGeom(unittest.TestCase):
+
+	def setUp(self):
+		self.waterDetector = mock.Mock()
+		self.eleToTypeIdx = {"O":2,"H":1}
+		self.eleToCharge = {"O":4,"H":5}
+		self.eleToMass = None
+		self.convAngToBohr = True
+		self.modTiltFactors = [2,2,2]
+		self.uCellObj = mock.Mock()
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		args = [self.waterDetector, self.eleToTypeIdx]
+		kwargs = {"eleToCharge":self.eleToCharge,"convBohrToAng":self.convAngToBohr,
+		          "modTiltFactors":self.modTiltFactors, "eleToMass":self.eleToMass}		           
+		self.testObjA = tCode.PureWaterCellToLammpsGeomMapperCentralOnly(*args, **kwargs)
+
+	@mock.patch("gen_basis_helpers.lammps_interface.pure_water_map_objs.copy.deepcopy")
+	@mock.patch("gen_basis_helpers.lammps_interface.pure_water_map_objs.lammpsGeomHelp")
+	def testExpectedCallsForSimpleCase(self, mockedGeomHelp, mockedDeepCopy):
+		#Figure out expected values
+		expOutObj = mock.Mock()
+		expDataDictFunct = mock.Mock()
+		expGeomToBondInfo    = tCode.GetPureWaterBondsFromInpGeomCentralCellOnly(self.waterDetector)
+		expGeomToAngleInfo   = tCode.GetPureWaterAnglesFromInpGeomCentralCellOnly(self.waterDetector)
+		expGeomToMoleculeIDs = tCode.GetPureWaterMoleculeIndicesFromInpGeomCentralCellOnly(self.waterDetector)
+		expKwargDictLammpsGeom = {"eleToTypeIdx":self.eleToTypeIdx, "eleToCharge":self.eleToCharge, "eleToMass":self.eleToMass,
+		                          "geomToBondInfo":expGeomToBondInfo, "geomToAngleInfo":expGeomToAngleInfo,
+		                          "geomToMoleculeIDs":expGeomToMoleculeIDs, "getDataDictFunct":expDataDictFunct}
+
+		#Mock the relevant function
+		mockedGeomHelp.LammpsGeom.side_effect = lambda *args,**kwargs: expOutObj
+		mockedGeomHelp.GetDataDictFromLammpsGeomAtomStyleFull.side_effect = lambda *args,**kwargs:expDataDictFunct
+		mockedDeepCopy.side_effect = lambda x:x
+
+		#Run + check expected calls made
+		actOutObj = self.testObjA(self.uCellObj)
+		mockedGeomHelp.GetDataDictFromLammpsGeomAtomStyleFull.assert_called_with(modTiltFactors=self.modTiltFactors)
+		mockedGeomHelp.LammpsGeom.assert_called_with(self.uCellObj, **expKwargDictLammpsGeom)
+		self.uCellObj.convBohrToAng.assert_called_with()
+
+		self.assertEqual(expOutObj,actOutObj)
+
 
 class TestGetBondsFromInpGeom(unittest.TestCase):
 
@@ -36,6 +82,19 @@ class TestGetBondsFromInpGeom(unittest.TestCase):
 		self.createTestObjs()
 		with self.assertRaises(AssertionError):
 			self.testObjA(self.cellA)
+
+	def testEqualObjsCompareEqual(self):
+		objA = copy.deepcopy(self.testObjA)
+		self.createTestObjs()
+		objB = self.testObjA
+		self.assertEqual(objA,objB)
+
+	def testUnequalObjsCompareEqual(self):
+		objA = copy.deepcopy(self.testObjA)
+		self.minOHDist += 0.1
+		self.createTestObjs()
+		objB = self.testObjA
+		self.assertNotEqual(objA,objB)
 
 class TestGetAnglesFromInpGeom(unittest.TestCase):
 
