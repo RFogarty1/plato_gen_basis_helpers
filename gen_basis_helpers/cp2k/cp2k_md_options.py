bc6348dfe98901797e6078db35cfeb392624c041
Richard Fogarty <richard.m.fogarty@gmail.com>
2020-12-17 13:10:15 +0000

    cp2k: Added thermostat type option for md

--- gen_basis_helpers/cp2k/cp2k_md_options.py
+++ gen_basis_helpers/cp2k/cp2k_md_options.py
@@ -12,7 +12,7 @@ class MolDynamicsOptsCP2KBase():
 
 class MolDynamicsOptsCP2KStandard():
 
-	def __init__(self, timeStep=None, nSteps=None, ensemble=None, temperature=None):
+	def __init__(self, timeStep=None, nSteps=None, ensemble=None, temperature=None, thermostatType=None):
 		""" Initializer
 		
 		Args:
@@ -20,7 +20,8 @@ class MolDynamicsOptsCP2KStandard():
 			nSteps: (int) Number of steps to run
 			ensemble: (str) e.g. NVE, NPT. Passed directly to ensemble keyword in Motion/MD section
 			temperature: (float) In Kelvin
-				 
+			thermostatType: (Str) e.g. Nose
+ 
 		Returns
 			What Function Returns
 	 
@@ -31,6 +32,7 @@ class MolDynamicsOptsCP2KStandard():
 		self.nSteps = nSteps
 		self.ensemble = ensemble
 		self.temperature = temperature
+		self.thermostatType = thermostatType
 
 		#Mapping from numbers to strings
 		self.tempFmt = "{:.2f}"
@@ -40,7 +42,8 @@ class MolDynamicsOptsCP2KStandard():
 	def optDict(self):
 		outDict = {"mdEnsemble": self.ensemble, "mdSteps": self.nSteps,
 		           "mdTimeStep": self.timeStepFmt.format(self.timeStep) if self.timeStep is not None else None,
-		           "mdTemperature": self.tempFmt.format(self.temperature) if self.temperature is not None else None}
+		           "mdTemperature": self.tempFmt.format(self.temperature) if self.temperature is not None else None,
+	               "mdThermostatType": self.thermostatType if self.thermostatType is not None else None}
 
 		outDict = {k:v for k,v in outDict.items() if v is not None}
 
