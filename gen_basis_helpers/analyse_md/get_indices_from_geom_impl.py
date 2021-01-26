19735ea2e6b285f971ee41e60ae055c9d2d5ddb2
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-01-26 12:57:00 +0000

    analyse_md: Fixed bug in finding water indices AND added code for finding only top-layer of water indices
    Bug was OHH angle used instead of HOH

--- gen_basis_helpers/analyse_md/get_indices_from_geom_impl.py
+++ gen_basis_helpers/analyse_md/get_indices_from_geom_impl.py
@@ -1,9 +1,15 @@
 
+import copy
+import itertools as it
+
+import plato_pylib.shared.ucell_class as uCellHelp
 import plato_pylib.shared.unit_convs as uConvHelp
 
 from . import get_indices_from_geom_core as getIdxCore
 from . import get_neb_lists as nebListHelp
 from . import calc_dists as calcDistHelp
+
+from ..shared import cart_coord_utils as cartHelp
 
 class GetSurfaceIndicesFromGeomStandard(getIdxCore.GetSpecialIndicesFromInpGeomTemplate):
 
@@ -20,10 +26,50 @@ class GetSurfaceIndicesFromGeomStandard(getIdxCore.GetSpecialIndicesFromInpGeomT
 		return [eleFilter, surfFilter]
 
 
+class GetTopWaterLayerIndices():
+
+	def __init__(self, waterDetector=None, maxHeightLayer=uConvHelp.ANG_TO_BOHR, top=True):
+		""" Initializer
+		
+		Args:
+			waterDetector: (GetWaterMoleculeIndicesFromGeomStandard object) Used to get indices related to water
+			maxLayerHeight: (float) Difference in OXYGEN atom heights between top/bottom of the layer
+			top: (Bool) If True then look for the O at the top of the surface, if False look for the O at the bottom of the surface
+				 
+		Returns
+			outIndices: (iter of len-3 iters) The indices corresponding to the top(or bottom) layer of water molecules
+	 
+		"""
+		self.waterDetector = GetWaterMoleculeIndicesFromGeomStandard() if waterDetector is None else waterDetector
+		self.maxHeightLayer = maxHeightLayer
+		self.top = top
+
+	def getIndicesFromInpGeom(self, inpGeom):
+		waterIndices = self.waterDetector.getIndicesFromInpGeom(inpGeom)
+		allFractCoords = inpGeom.fractCoords
+		allWaterAtomIndices = [x for x in it.chain(*waterIndices)]
+		relFractCoords = [x for idx,x in enumerate(allFractCoords) if idx in allWaterAtomIndices and x[-1].upper()=="O"]
+
+		#TODO: Probably have an "optimisation" option to skip this bit; the purpose of which is mainly to make sure
+		#we deal with PBCs properly
+		useCell = copy.deepcopy(inpGeom)
+		useCell.fractCoords = relFractCoords
+		uCellHelp.foldAtomicPositionsIntoCell(useCell)
+
+		cartCoords = useCell.cartCoords
+
+		planeEqn = cartHelp.getPlaneEqnForOuterSurfaceAtoms(useCell, top=self.top)
+		distsFromPlane = [ planeEqn.getDistanceOfPointFromPlane(x[:3]) for x in cartCoords]
+		outWaterIndices = list()
+		for idx,val in enumerate(distsFromPlane):
+			if val < self.maxHeightLayer:
+				outWaterIndices.append( waterIndices[idx] )
+
+		return outWaterIndices
 
 class GetWaterMoleculeIndicesFromGeomStandard():
 
-	def __init__(self, minOH=0.01, maxOH=1.2*uConvHelp.ANG_TO_BOHR, minAngle=105, maxAngle=115):
+	def __init__(self, minOH=0.01, maxOH=1.2*uConvHelp.ANG_TO_BOHR, minAngle=100, maxAngle=110):
 		self.minOH = minOH
 		self.maxOH = maxOH
 		self.minAngle = minAngle
@@ -43,7 +89,7 @@ class GetWaterMoleculeIndicesFromGeomStandard():
 					oCoord, hCoordA, hCoordB = [ cartCoords[idx][:3] for idx in [oIdx]+currNebList ]
 					hDistA = calcDistHelp.calcSingleDistBetweenCoords_minImageConv(inpGeom, oCoord, hCoordA)
 					hDistB = calcDistHelp.calcSingleDistBetweenCoords_minImageConv(inpGeom, oCoord, hCoordB)
-					angle = calcDistHelp.calcSingleAngleBetweenCoords_minImageConv(inpGeom, oCoord, hCoordA, hCoordB)
+					angle = calcDistHelp.calcSingleAngleBetweenCoords_minImageConv(inpGeom, hCoordA, oCoord, hCoordB)
 					if self._areDistsAndAnglesConsistent([hDistA,hDistB], angle):
 						outIndices.append( [oIdx] + currNebList )
 
--- gen_basis_helpers/analyse_md/unit_tests/utests_get_indices_impl.py
+++ gen_basis_helpers/analyse_md/unit_tests/utests_get_indices_impl.py
@@ -47,8 +47,8 @@ class TestGetWaterMoleculeIndicesStandard(unittest.TestCase):
 		#Parameters for the "molecule finder"
 		self.minOH = 0.5
 		self.maxOH = 2.0
-		self.minAngle = 40
-		self.maxAngle = 50
+		self.minAngle = 80
+		self.maxAngle = 100
 
 		self.createTestObjs()
 
@@ -75,17 +75,82 @@ class TestGetWaterMoleculeIndicesStandard(unittest.TestCase):
 		self.assertEqual(expOutput, actOutput)
 
 	def testExpected_angleWrong(self):
-		self.minAngle = 90
-		self.maxAngle = 150
+		self.minAngle = 10
+		self.maxAngle = 20
 		self.createTestObjs()
 		expOutput = list()
 		actOutput = self._runTestFunct()
 		self.assertEqual(expOutput,actOutput)
 
 
+#Simply mock the water index getter and stack 3 water molecules on top of each other
+class TestGetTopWaterLayerIndicesA(unittest.TestCase):
 
+	def setUp(self):
+		self.lattParams = [10,10,10]
+		self.lattAngles = [90,90,90]
+		self.waterA = [ [5, 5, 5  ,"O"],
+		                [5, 5, 5.5,"H"],
+		                [5, 5, 4.5,"H"] ]
 
+		self.waterB = [ [3, 3, 4  , "O"],
+		                [3, 3, 3.5, "H"],
+		                [3, 3, 3.5, "H"] ]
 
+		self.waterC = [ [8, 8, 6  , "O"],
+		                [8, 8, 6.5, "H"],
+		                [8, 8, 5.5, "H"] ]
+
+		self.waterIndices = [ [0,1,2], [3,4,5] , [6,7,8] ]
+
+		self.maxLayerHeight = 0.5
+		self.top = True
+
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.coordsA = list()
+		self.coordsA.extend(self.waterA)
+		self.coordsA.extend(self.waterB)
+		self.coordsA.extend(self.waterC)
+
+		self.cellA = uCellHelp.UnitCell(lattParams=self.lattParams, lattAngles=self.lattAngles)
+		self.cellA.cartCoords = self.coordsA
+
+		self.waterDetector = mock.Mock()
+		self.waterDetector.getIndicesFromInpGeom.side_effect = lambda *args,**kwargs: self.waterIndices
+
+		self.testObjA = tCode.GetTopWaterLayerIndices(waterDetector=self.waterDetector, maxHeightLayer=self.maxLayerHeight, top=self.top)
+
+	def _runTestFunct(self):
+		return self.testObjA.getIndicesFromInpGeom(self.cellA)
+
+	def testExpRes_pbcsDontMatter(self):
+		expIndices = [ [6,7,8] ]
+		actIndices = self._runTestFunct()
+		self.assertEqual(expIndices,actIndices)
+
+	def testExpRes_pbcsImportant(self):
+		self.waterC[0][2] -= self.lattParams[-1]
+		self.createTestObjs()
+
+		expIndices = [ [6,7,8] ]
+		actIndices = self._runTestFunct()
+		self.assertEqual(expIndices,actIndices)
+
+	def testExpRes_twoLayersExpected(self):
+		self.maxLayerHeight = 1.5
+		self.createTestObjs()
+		expIndices = [ [0,1,2], [6,7,8] ]
+		actIndices = self._runTestFunct()
+		self.assertEqual(expIndices,actIndices)
+
+	def testExpRes_bottom(self):
+		self.top = False
+		self.createTestObjs()
+		expIndices = [ [3,4,5] ]
+		actIndices = self._runTestFunct()
+		self.assertEqual(expIndices,actIndices)
 
 
 
