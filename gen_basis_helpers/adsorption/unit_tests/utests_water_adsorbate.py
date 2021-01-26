16a6cd00559f74856d74dbeed681ed7447663f1e
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-01-26 12:57:06 +0000

    adsorption/water_adsorbate: Multiple changes; including bug fix in getting azimuthal for water xyz when OH dists unequal
    Can now also get adsorbate objs from xyz; standard adsorption obj has __eq__ implemented; basic code to get next water bilayer added

--- gen_basis_helpers/adsorption/unit_tests/utests_water_adsorbate.py
+++ gen_basis_helpers/adsorption/unit_tests/utests_water_adsorbate.py
@@ -1,8 +1,11 @@
 
+import copy
 import itertools as it
 import math
 import unittest
 import unittest.mock as mock
+
+import plato_pylib.shared.ucell_class as uCellHelp
 
 import gen_basis_helpers.adsorption.water_adsorbate as tCode
 
@@ -243,3 +246,187 @@ class TestGetAxisRotationsFromInpXyz(unittest.TestCase):
 		self.waterObjA.roll = -89.999
 		with self.assertRaises(ValueError):
 			tCode.getStandardAxisRotationsFromXyz(self.waterObjA.geom)
+
+
+
+class CheckExpAndActEqualMixin():
+
+	def _checkExpAndActCoordsEqual(self, expCoords, actCoords):
+		self.assertEqual( len(expCoords), len(actCoords) )
+		for exp,act in zip(expCoords,actCoords):
+			self.assertEqual( len(act), 4 )
+			self.assertEqual(exp[-1],act[-1])
+			[self.assertAlmostEqual(e,a) for e,a in zip(exp[:3],act[:3])]
+
+
+
+class TestGetStandardWaterAdsObjFromXYZCoords(unittest.TestCase, CheckExpAndActEqualMixin):
+
+	def setUp(self):
+		self.ohDists = [1,2] #Different ohDists must slightly alter the azimuth value
+		self.angle = 90
+		self.pitch = 12
+		self.roll = 25
+		self.azimuthal = 18
+		self.translationVector = [1,1,1]
+		self.refPosGetter = tCode.WaterRefPosGetterStandard()
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		kwargs = {"refPosGetter":self.refPosGetter, "pitch":self.pitch, "roll":self.roll,
+		          "azimuthal":self.azimuthal, "translationVector":self.translationVector}
+		self.testObjA = tCode.WaterAdsorbateStandard(self.ohDists, self.angle, **kwargs)
+		self.testXyzA = self.testObjA.geom
+
+	def testExpWaterObjA(self):
+		expXyz = self.testXyzA
+		actObj = tCode.getStandardWaterAdsObjFromXyz(expXyz)
+		self._checkExpAndActCoordsEqual(expXyz, actObj.geom)
+
+
+#This means messing with WaterRefPosGetter return co-ords for the OXYGEN
+class TestGetWaterAdsorptionObjsFromCellAndIndices(unittest.TestCase, CheckExpAndActEqualMixin):
+
+	def setUp(self):
+		self.lattParamsA = [10,10,10]
+		self.lattAnglesA = [90,90,90]
+
+		self.indicesA = [ [1,2,3] ]
+		self.coordsA = [ [3,3,3,"X"], [4,4,9,"O"], [3.5,3.5,9,"H"], [4,3.5,9,"H"] ] 
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.cellA = uCellHelp.UnitCell(lattParams=self.lattParamsA, lattAngles=self.lattAnglesA)
+		self.cellA.cartCoords = self.coordsA
+
+	def _runTestFunct(self):
+		return tCode.getWaterAdsorptionObjsFromInpCellAndWaterIndices(self.cellA, self.indicesA)
+
+	def testExpA_pbcsIrrelevant(self):
+		expObjs = [ tCode.getStandardWaterAdsObjFromXyz(self.coordsA[1:]) ]
+		expGeom = expObjs[0].geom
+		actObjs = self._runTestFunct()
+		actGeom = actObjs[0].geom
+		self._checkExpAndActCoordsEqual(expGeom, actGeom)
+
+	def testExpB_pbcsMatter(self):
+		expObjs = [ tCode.getStandardWaterAdsObjFromXyz(self.coordsA[1:]) ]
+		self.coordsA[2][-2] += self.lattParamsA[-1]
+		self.createTestObjs()
+
+		expGeom = expObjs[0].geom
+		actObjs = self._runTestFunct()
+		actGeom = actObjs[0].geom
+		self._checkExpAndActCoordsEqual(expGeom, actGeom)
+
+
+class TestWaterAdsorbateEquality(unittest.TestCase):
+
+	def setUp(self):
+		self.ohDists = [1,2] #Different ohDists must slightly alter the azimuth value
+		self.angle = 90
+		self.pitch = 12
+		self.roll = 25
+		self.azimuthal = 18
+		self.translationVector = [1,1,1]
+		self.refPosGetter = tCode.WaterRefPosGetterStandard()
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		kwargs = {"refPosGetter":self.refPosGetter, "pitch":self.pitch, "roll":self.roll,
+		          "azimuthal":self.azimuthal, "translationVector":self.translationVector}
+		self.testObjA = tCode.WaterAdsorbateStandard(self.ohDists, self.angle, **kwargs)
+
+	def testEqualObjsCompareEqual(self):
+		objA = copy.deepcopy(self.testObjA)
+		self.createTestObjs()
+		objB = self.testObjA
+		self.assertEqual(objA, objB)
+
+	def testUnequalObjsCompareUnequal_diffAngle(self):
+		objA = copy.deepcopy(self.testObjA)
+		self.angle += 12
+		self.createTestObjs()
+		objB = self.testObjA
+		self.assertNotEqual(objA, objB)
+
+	def testUnequalObjsCompareUnequal_difftranslationVector(self):
+		objA = copy.deepcopy(self.testObjA)
+		self.translationVector[-1] += 2
+		self.createTestObjs()
+		objB = self.testObjA
+		self.assertNotEqual(objA, objB)
+
+
+class TestGetAdsorbatesForNextWaterBilayer(unittest.TestCase):
+
+	def setUp(self):
+		#The "flat" one
+		self.ohDistsFlat, self.angleFlat = [1.2, 1.2], 110
+		self.pitchFlat, self.rollFlat, self.azimuthFlat = 10, 12, 20
+		self.tVectFlat =  [2,3,4]
+
+		#The "Hup" one
+		self.ohDistsHup, self.angleHup = [1.1, 1.1], 105
+		self.pitchHup, self.rollHup, self.azimuthHup = 30, 80, 30
+		self.tVectHup = [3,4,5]
+
+		#Args for the constructor object in essence
+		self.bilayerSpacing = 2
+		self.top = True
+		self.surfNormal = [0,0,1]
+		self.layerTol = 0.5
+
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		kwargsFlat = {"pitch": self.pitchFlat, "roll": self.rollFlat,
+		              "azimuthal": self.azimuthFlat, "translationVector": self.tVectFlat}
+		kwargsHup = {"pitch": self.pitchHup, "roll": self.rollHup,
+		             "azimuthal": self.azimuthHup, "translationVector": self.tVectHup}
+		self.flatAdsObj = tCode.WaterAdsorbateStandard(self.ohDistsFlat, self.angleFlat, **kwargsFlat)
+		self.hupAdsObj  = tCode.WaterAdsorbateStandard(self.ohDistsHup , self.angleHup , **kwargsHup)
+		self.adsObjs = [self.flatAdsObj, self.hupAdsObj]
+
+	def _runTestFunct(self):
+		args = [self.adsObjs, self.bilayerSpacing]
+		kwargDict = {"layerTolerance":self.layerTol , "top": self.top, "surfNormal": self.surfNormal}
+		return tCode.getAdsorbateObjsForNextWaterBilayerBasic(*args, **kwargDict)
+
+	def _loadExpObjsA(self):
+		layerHeight = abs( self.tVectHup[-1] - self.tVectFlat[-1] )
+
+		#First is on top of the "flat" water molecule
+		expAdsObjA = copy.deepcopy(self.hupAdsObj)
+		expAdsObjA.translationVector = copy.deepcopy(self.tVectFlat)
+		expAdsObjA.translationVector[-1] += self.bilayerSpacing + (2*layerHeight)
+		expAdsObjA.azimuthal += 180
+
+		#Second is on top of the "Hup" water molecule
+		expAdsObjB = copy.deepcopy(self.flatAdsObj)
+		expAdsObjB.translationVector = copy.deepcopy(self.tVectHup)
+		expAdsObjB.translationVector[-1] += self.bilayerSpacing
+		expAdsObjB.azimuthal += 180
+
+		return [expAdsObjB, expAdsObjA]
+
+	def testExpectedValuesA(self):
+		expObjs = self._loadExpObjsA()
+		actObjs = self._runTestFunct()
+		self.assertEqual(expObjs,actObjs)
+
+	def testRaisesIfTopAndBottomOfBilayerIndistinghuisable(self):
+		self.layerTol = 2
+		with self.assertRaises(ValueError):
+			self._runTestFunct()
+
+	def testRaisesIfMoreThanTwoLayers(self):
+		extraAds = copy.deepcopy(self.flatAdsObj)
+		extraAds.translationVector[-1] += 5
+
+		self.adsObjs += [extraAds]
+		with self.assertRaises(ValueError):
+			self._runTestFunct()
+
+
+
--- gen_basis_helpers/adsorption/water_adsorbate.py
+++ gen_basis_helpers/adsorption/water_adsorbate.py
@@ -1,13 +1,46 @@
 
+import copy
 import itertools as it
 import math
 
 import numpy as np
 
+import plato_pylib.shared.unit_convs as uConvHelp
+
 from . import adsorbate_rep_objs as adsObjHelp
 
+from ..analyse_md import calc_dists as distsHelp
 from ..shared import plane_equations as planeEqnHelp
 from ..shared import simple_vector_maths as vectHelp
+
+
+def getStandardWaterAdsObjFromXyz(inpXyz, surfNormal=None):
+	""" Creates a WaterAdsorbateStandard object from input xyz co-ords
+	
+	Args:
+		inpXyz: (len-3 iter of len-4 iters) Cartesian co-ords for water. E.g. [ [0,0,0,"O"], [0,0.7,0.7,"H"], [0,0.7,-0.7,"H"] ]
+		surfNormal: (len-3 iter) Normal vector for the surface, used to define the azimuthal axis. Only ever tested with default of [0,0,1]
+ 
+	Returns
+		 outObj: (WaterAdsorbateStandard) 
+ 
+	"""
+	surfNormal = [0,0,1] if surfNormal is None else surfNormal
+
+	dists, angle = getWaterInternalCoordsFromXYZ(inpXyz)
+	surfNormal = [0,0,1]
+	roll, pitch, azimuth = getStandardAxisRotationsFromXyz(inpXyz, surfNormal=surfNormal)
+	refPosGetter = WaterRefPosGetterStandard()
+	oIndices = [idx for idx,x in enumerate(inpXyz) if x[-1].upper()=="O"]
+	assert len(oIndices)==1
+	translationVector = inpXyz[oIndices[0]][:3]
+
+	kwargDict = {"refPosGetter":refPosGetter, "pitch":pitch, "roll":roll,
+	             "azimuthal":azimuth, "translationVector": translationVector}
+	outObj = WaterAdsorbateStandard(dists, angle, **kwargDict)
+
+	return outObj
+
 
 class WaterAdsorbateStandard(adsObjHelp.Adsorbate):
 
@@ -24,6 +57,7 @@ class WaterAdsorbateStandard(adsObjHelp.Adsorbate):
 			azimuthal: (Float, Optional, default=0) Rotation about the z-axis
 			translationVector: (Float, len-3 iter, default=[0,0,0]): Apply this translation to all atoms 
 		"""
+		self._eqTol = 1e-5
 		self.ohDists = list(ohDists)
 		self.angle = angle
 		self.refPosGetter = refPosGetter if refPosGetter is not None else WaterRefPosGetterStandard()
@@ -67,6 +101,28 @@ class WaterAdsorbateStandard(adsObjHelp.Adsorbate):
 		for idx, coords in enumerate(newCoords):
 			geom[idx][:3] = coords[:3]
 
+	def __eq__(self, other):
+		eqTol = min(self._eqTol, other._eqTol)
+		floatAttrs = ["angle", "pitch", "roll", "azimuthal"]
+		floatListAttrs = ["ohDists", "translationVector"]
+
+		if self.refPosGetter != other.refPosGetter:
+			return False
+
+		for attr in floatAttrs:
+			valA, valB = getattr(self, attr), getattr(other,attr)
+			if abs(valA-valB) > eqTol:
+				return False
+
+		for attr in floatListAttrs:
+			valsA, valsB = getattr(self,attr), getattr(other,attr)
+			diffs = [abs(x-y) for x,y in it.zip_longest(valsA,valsB)]
+			if any([x>eqTol for x in diffs]):
+				return False
+
+		return True
+
+
 class WaterRefPosGetterStandard():
 	""" Takes internal co-ordinates for water and converts into a set of cartesian co-ordinates. The callable interface is as follows:
 
@@ -76,7 +132,7 @@ class WaterRefPosGetterStandard():
 
 	"""
 	def __init__(self):
-		pass
+		self._eqTol = 1e-5
 
 	@property
 	def rollAxis(self):
@@ -102,6 +158,18 @@ class WaterRefPosGetterStandard():
 			currVals = [x,y,0,symbol]
 			outVals.append(currVals)
 		return outVals		
+
+	def __eq__(self, other):
+		if type(self) != type(other):
+			return False
+
+		eqTol = min(self._eqTol, other._eqTol)
+		props = ["rollAxis", "pitchAxis", "azimuthalAxis"]
+		for prop in props:
+			diffs = [abs(x-y) for x,y in zip(getattr(self,prop), getattr(other,prop))]
+			if any([x>eqTol for x in diffs]):
+				return False
+		return True
 
 
 def getWaterInternalCoordsFromXYZ(inpCoords):
@@ -228,6 +296,7 @@ def _getAzimuthVectorForInpWaterCoords(inpCoords, surfVector=None):
 
 def _getBisectVectorForInpWaterCoords(inpCoords):
 	vectA, vectB = _getOHVectorsFromInpCoords(inpCoords)
+	vectA, vectB = [vectHelp.getUnitVectorFromInpVector(x) for x in [vectA,vectB]]
 	outVect = [a+b for a,b in it.zip_longest(vectA,vectB)]
 	return outVect
 
@@ -239,4 +308,130 @@ def _getOHVectorsFromInpCoords(inpCoords):
 	return [vectA, vectB]
 
 
+def getWaterAdsorptionObjsFromInpCellAndWaterIndices(inpCell, indices):
+	""" Gets a list of WaterAdsorbateStandard from an inputCell and indices of water molecules
+	
+	Args:
+		inpCell: (plato_pylib UnitCell object)
+		indices: (iter of len-3 iters) Each contains indices for a single water molecule
+			 
+	Returns
+		 outObjs: (iter of WaterAdsorbateStandard objs) The cartesian co-ords for the Oxygen will be unchanged, the Hydrogen will be the images closest to the oxygen 
+ 
+	"""
+	lattVects = inpCell.lattVects
+	cartCoords = inpCell.cartCoords
 
+	outObjs = list()
+	for currIndices in indices:
+		currStartCoords = [cartCoords[idx] for idx in currIndices]
+		currXYZ = _getOCentredWaterCoords_minImageConv(inpCell, currStartCoords)
+		currObj = getStandardWaterAdsObjFromXyz(currXYZ)
+		outObjs.append(currObj)
+
+	return outObjs
+
+def _getOCentredWaterCoords_minImageConv(inpCell, waterCoords):
+	#Get indices for O/H; so that we can centre on O
+	oIndices = [idx for idx,x in enumerate(waterCoords) if x[-1].upper()=="O"]
+	hIndices = [idx for idx,x in enumerate(waterCoords) if x[-1].upper()=="H"]
+	assert len(oIndices)==1
+	assert len(hIndices)==2
+	
+	#Get co-ords by minimum image convention (i think....)
+	oCoords = waterCoords[oIndices[0]]
+	outCoords = [ oCoords ]
+	for idx in hIndices:
+		currStartCoords = waterCoords[idx]
+		currOutCoords = distsHelp.getNearestImageNebCoordsBasic( inpCell, oCoords[:3], waterCoords[idx][:3] ) + ["H"]
+		outCoords.append(currOutCoords)
+
+	return outCoords
+
+
+#TODO: Add topLayerAdsObj=None. Allow user to pass an adsorbate obj (ignoring translation vector) that will replace the 
+def	getAdsorbateObjsForNextWaterBilayerBasic(inpAds, bilayerSpacing, layerTolerance=0.2*uConvHelp.ANG_TO_BOHR, top=True, surfNormal=None, topLayerTemplate=None):
+	""" Gets a list of adsorbate objects which form the next water bilayer for a structure
+	
+	Args:
+		inpAds: (iter of WaterAdsorbateStandard objs) Each represents one water molecule in the top bilayer (we add either above/below this)
+		bilayerSpacing: (float) The closest gap between OXYGEN atoms between two bilayers
+		layerTolerance: (float) The maximum distance between two oxygens for them to be considered in the same single layer of a bilayer [should be set pretty small]
+		surfNormal: [len-3 iter] [x,y,z] for the surface normal vector. Default = [0,0,1]
+		topLayerTemplate: (WaterAdsorbateStandard obj) By default the code will create the new layers by transforming the two types of water adsorbates (flat/Hup or Hdown) in the inpAds objects. If you set topLayerTemplate the code will act as if THIS adsorbate was found at the top of the inpAds objs bilayer structure. This is useful for creating extra adsorbate layers on HDown structures but using Hup configurations for the next layers
+	
+	NOTE:
+		All inpsAds objects must be using the default WaterRefPosGetterStandard, which has the oxygen at origin in the "standard" orientation
+
+	Returns
+		outAdsObjs: (iter of WaterAdsorbateStandard objs) 
+ 
+	Raises:
+		 ValueError: If inpAds span more than 2 layers. Layers are defined using layerTolerance
+	"""
+
+	surfNormal = [0,0,1] if surfNormal is None else vectHelp.getUnitVectorFromInpVector(surfNormal)
+
+	#1) Figure out the average height between each "monolayer" making up the bilayer
+	oCoords = list()
+	for adsObj in inpAds:
+		currCoords = [x for x in adsObj.geom if x[-1].upper()=="O"]
+		assert len(currCoords)==1
+		oCoords.append(currCoords[0])
+
+	planeEqn = planeEqnHelp.ThreeDimPlaneEquation(*surfNormal,0)
+	signedODists = [planeEqn.getSignedDistanceOfPointFromPlane(x[:3]) for x in oCoords]
+	botDist, topDist = min(signedODists), max(signedODists)
+
+	#1.5) Divide the bilayer into a "top" and "bottom" layer
+	botIndices, topIndices = list(), list()
+	for idx,dist in enumerate(signedODists):
+		distFromBot, distFromTop = abs(dist-botDist), abs(dist-topDist)
+		if distFromBot<layerTolerance:
+			botIndices.append(idx)
+		if distFromTop<layerTolerance:
+			topIndices.append(idx)
+		if (distFromBot<layerTolerance) and (distFromTop<layerTolerance):
+			raise ValueError("Oxygen atom couldnt be assigned to either \"top\" or \"bottom\" of the bilayer. This likely means layerTolerance is set too high")
+
+	#TODO: Change to a ValueError
+	totalIndices = len(botIndices) + len(topIndices)
+	if totalIndices != len(signedODists):
+		raise ValueError("Assigned {} indices despite {} water present; This is likely due to either problems with the number of layers OR layerTolerance is set wrong".format(totalIndices,len(signedODists)))
+
+	#1.9) NOW we figure out the average gap between "top" and "bottom" layers
+	avTopLayerSignedDist = sum([signedODists[idx] for idx in topIndices]) / len(topIndices)
+	avBotLayerSignedDist = sum([signedODists[idx] for idx in botIndices]) / len(botIndices)
+	layerHeight = abs(avTopLayerSignedDist-avBotLayerSignedDist)
+
+	#2.5) Get the typical "top" and "bottom" orientations. They should all be pretty similar regardless.
+	#This is obviously a very imperfect way to do this; User supplying the "bottom" and "top" layer orientations will probably be the normal 
+	#way regardless though
+	topLayerTemplate = copy.deepcopy( inpAds[topIndices[0]] ) if topLayerTemplate is None else topLayerTemplate
+	botLayerTemplate = copy.deepcopy( inpAds[botIndices[0]] )
+
+	#3) Apply the relevant translations
+	if top:
+		firstLayerTVect = [bilayerSpacing*x for x in surfNormal]
+		secondLayerTVect = [(bilayerSpacing+(2*layerHeight))*x for x in surfNormal]
+		outAdsObjs = list()
+		#First sort out the closest layer
+		for idx in topIndices:
+			startTVect = inpAds[idx].translationVector
+			currAdsObj = copy.deepcopy(botLayerTemplate) #NEarest to top layer, so orientation flips to "bot-layer like"
+			currAdsObj.translationVector = [x+y for x,y in zip(startTVect, firstLayerTVect)]
+			currAdsObj.azimuthal += 180
+			outAdsObjs.append(currAdsObj)
+
+		for idx in botIndices:
+			startTVect = inpAds[idx].translationVector
+			currAdsObj = copy.deepcopy(topLayerTemplate)
+			currAdsObj.translationVector = [x+y for x,y in zip(startTVect, secondLayerTVect)]
+			currAdsObj.azimuthal += 180
+			outAdsObjs.append(currAdsObj)
+
+	else:
+		raise NotImplementedError("")
+
+
+	return outAdsObjs
