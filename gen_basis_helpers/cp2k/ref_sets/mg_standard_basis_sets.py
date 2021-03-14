1d80dd442b81a381e54d186ddd64be9838dd412e
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-03-14 15:06:57 +0000

    cp2k: Added some q10 pseudopotential basis sets for Mg

--- gen_basis_helpers/cp2k/ref_sets/mg_standard_basis_sets.py
+++ gen_basis_helpers/cp2k/ref_sets/mg_standard_basis_sets.py
@@ -4,7 +4,7 @@
 from .. import basis_register as basRegister
 from .. import cp2k_basis_obj as basObj
 
-
+#MOLOPT BASIS SETS
 @basRegister.decoRegisterCP2KBasisCreatorToMethodStr("GTH-PBE-q2-MolOpt-UCL-SZV".lower())
 def _getMgMolOptUclSZVBasis():
 	return basObj.CP2KBasisObjStandard( element="Mg", basis="SZV-MOLOPT-SR-GTH-q2",
@@ -32,6 +32,35 @@ def _getMgMolOptUclTZVP_basis():
 def _getMgMolOptUclTZVP_basis():
 	return basObj.CP2KBasisObjStandard( element="Mg", basis="TZV2P-MOLOPT-SR-GTH-q2",
 	                             potential="GTH-PBE-q2", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT_UCL" )
+
+
+
+
+#q10 Pseudopotential molopt basis sets
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("Mg-GTH-PBE-q10-MolOpt-DZVP".lower())
+def _unused():
+	return basObj.CP2KBasisObjStandard( element="Mg", basis="DZVP-MOLOPT-SR-GTH-q10",
+	                                    potential="GTH-PBE-q10", potFile="GTH_POTENTIALS", basisFile="BASIS_MOLOPT" )
+
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("Mg-GTH-PBE-q10-DZVP".lower())
+def _unused():
+	return basObj.CP2KBasisObjStandard( element="Mg", basis="DZVP-GTH-q10",
+	                                    potential="GTH-PBE-q10", potFile="GTH_POTENTIALS", basisFile="GTH_BASIS_SETS" )
+
+
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("Mg-GTH-PBE-q10-TZVP".lower())
+def _unused():
+	return basObj.CP2KBasisObjStandard( element="Mg", basis="TZVP-GTH-q10",
+	                                    potential="GTH-PBE-q10", potFile="GTH_POTENTIALS", basisFile="GTH_BASIS_SETS" )
+
+@basRegister.decoRegisterCP2KBasisCreatorToMethodStr("Mg-GTH-PBE-q10-TZV2P".lower())
+def _unused():
+	return basObj.CP2KBasisObjStandard( element="Mg", basis="TZV2P-GTH-q10",
+	                                    potential="GTH-PBE-q10", potFile="GTH_POTENTIALS", basisFile="GTH_BASIS_SETS" )
+
+
+
+
 
 
 
