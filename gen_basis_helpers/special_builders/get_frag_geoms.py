0cc22f5836132646b715516ce0bf4f8e1d0b33aa
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-03-31 11:45:32 +0100

    special_builders: Added functions for getting fragment geometries from an inout geometry + indices
    Useful for getting interactions between fragments in a system

---
+++ gen_basis_helpers/special_builders/get_frag_geoms.py
@@ -0,0 +1,62 @@
+
+
+import copy
+
+
+def getIsolatedFragGeomsFromFragmentIndices(inpGeom, fragIndices):
+	""" Returns iter of geometries created from fragments of inpGeom
+	
+	Args:
+		inpGeom: (plato_pylib UnitCell instance)
+		fragIndices: (iter of iter of ints) List of indices to use in each output geometry
+			 
+	Returns
+		outGeoms: (iter of UnitCell instance) All have the same cell, but different contents, as inpGeom. The atoms placed in each are determined by fragIndices
+ 
+	"""
+
+	cartCoords = inpGeom.cartCoords
+	
+	outGeoms = list()
+	
+	#get isolated fragments
+	for frag in fragIndices:
+		currCell = copy.deepcopy(inpGeom)
+		currCell.cartCoords = [x for idx,x in enumerate(currCell.cartCoords) if idx in frag]
+		outGeoms.append(currCell)
+	
+	
+	return outGeoms
+	
+
+def getFragPairGeomsFromIndices(inpGeom, fragIndices, maxSep=None):
+	""" Description of function
+	
+	Args:
+		inpGeom: (plato_pylib UnitCell object)
+		fragIndices: (iter of iter of ints) List of indices to use in each output geometry
+		maxSep: (int) Maximum separation for creating pair-geom. For example geoms [A,B,C] with maxSep=1 means AB and BC will be returned, but not AC. The default (None) means return ALL pairs
+
+	Returns
+		outGeoms: (iter of UnitCell instance) All have the same cell, but different contents, as inpGeom. Order for simple example [A,B,C] is [AB,AC,BC]
+ 
+	"""
+	outGeoms = list()
+	for idxA,fragA in enumerate(fragIndices):
+		for idxB,fragB in enumerate(fragIndices[idxA+1:], start=idxA+1):
+			inclThisFrag = True
+
+			print("idxB-idxA = {}".format(idxB-idxA))
+			print("maxSep = {}".format(maxSep))
+			if (maxSep is not None):
+				if (idxB-idxA) > maxSep:
+					inclThisFrag = False
+			if inclThisFrag:
+				combinedIndices = fragA + fragB
+				currCell = copy.deepcopy(inpGeom)
+				currCell.cartCoords = [x for idx,x in enumerate(currCell.cartCoords) if idx in combinedIndices]
+				outGeoms.append(currCell)
+
+	return outGeoms
+
+
---
+++ gen_basis_helpers/special_builders/unit_tests/utests_get_frag_geoms.py
@@ -0,0 +1,71 @@
+
+import copy
+import unittest
+import unittest.mock as mock
+
+import plato_pylib.shared.ucell_class as uCellHelp
+
+import gen_basis_helpers.special_builders.get_frag_geoms as tCode
+
+class TestGetIsolatedFragGeomsFromFragIndices(unittest.TestCase):
+
+
+	def setUp(self):
+		self.fragIndices = [ [0,2], [1,3] ]
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.inpCell = _loadStandardCellA()
+
+	def _runTestFunct(self):
+		return tCode.getIsolatedFragGeomsFromFragmentIndices(self.inpCell, self.fragIndices)
+
+	def testExpectedGeomsA(self):
+		expCells = [copy.deepcopy(self.inpCell) for x in self.fragIndices]
+		expCells[0].cartCoords = [  [3,3,3,"X"], [5,5,5,"Z"] ]
+		expCells[1].cartCoords = [  [4,4,4,"Y"], [6,6,6,"A"] ]
+
+		actCells = self._runTestFunct()
+		self.assertEqual(expCells,actCells)
+
+
+class TestGetAllPairsOfFragGeomsFromFragIndices(unittest.TestCase):
+
+	def setUp(self):
+		self.fragIndices = [ [0], [1,3], [2] ]
+		self.maxSep = None
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.inpCell = _loadStandardCellA()
+
+	def _runTestFunct(self):
+		return tCode.getFragPairGeomsFromIndices(self.inpCell, self.fragIndices, maxSep=self.maxSep)
+
+	def testExpectedGeomsA_noSep(self):
+		expNumbGeoms = 3
+		expCells = [copy.deepcopy(self.inpCell) for x in range(expNumbGeoms)]
+		expCells[0].cartCoords = [ [3,3,3,"X"], [4,4,4,"Y"], [6,6,6,"A"] ]
+		expCells[1].cartCoords = [ [3,3,3,"X"], [5,5,5,"Z"] ]
+		expCells[2].cartCoords = [ [4,4,4,"Y"], [5,5,5,"Z"], [6,6,6,"A"] ]
+		actCells = self._runTestFunct()
+		self.assertEqual(expCells, actCells)
+
+	def testExpectedGeomsA_maxSepOne(self):
+		self.maxSep = 1
+		expNumbGeoms = 2
+		expCells = [copy.deepcopy(self.inpCell) for x in range(expNumbGeoms)]
+		expCells[0].cartCoords = [ [3,3,3,"X"], [4,4,4,"Y"], [6,6,6,"A"] ]
+		expCells[1].cartCoords = [ [4,4,4,"Y"], [5,5,5,"Z"], [6,6,6,"A"] ]
+		actCells = self._runTestFunct()
+		self.assertEqual(expCells, actCells)
+
+
+def _loadStandardCellA():
+	lattParams, lattAngles = [10,10,10], [90,90,90]
+	cartCoordsA = [ [3,3,3,"X"], [4,4,4,"Y"], [5,5,5,"Z"], [6,6,6,"A"] ]
+	currKwargs = {"lattParams":lattParams, "lattAngles":lattAngles}
+	inpCell = uCellHelp.UnitCell(**currKwargs)
+	inpCell.cartCoords = cartCoordsA
+	return inpCell
+
