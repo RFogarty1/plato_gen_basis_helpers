001e42e557dadde869d72eac1cd15353dc483098
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-04-27 16:05:23 +0100

    analyse_md: Added function to calculate distance matrices with nearest image conv
    Also added convenience function that exploits this to get the shortest distance between two sets of indices

--- gen_basis_helpers/analyse_md/calc_dists.py
+++ gen_basis_helpers/analyse_md/calc_dists.py
@@ -8,10 +8,54 @@ from . import mdanalysis_interface as mdAnalysisInter
 
 import plato_pylib.shared.ucell_class as uCellHelp
 
-#Will be too slow; we need to convert ALL the cart-coords.
-#def calcSingleDistBetweenCentralIndices_minImageConv(inpCell, idxA, idxB):
-#	cartCoords = inpCell.cartCoords
 
+def calcNearestDistanceBetweenTwoSetsOfIndices(inpCell, indicesA=None, indicesB=None, minDist=1e-2):
+	""" Calculates the nearest distance between atoms in indicesA and indicesB (uses nearest image convention)
+	
+	Args:
+		inpCell: (plato_pylib UnitCell object)
+		indicesA: (Optional, iter of ints) Indices of the atoms to include for the first dimension; Default is to include ALL atoms
+		indicesB: (Optional, iter of ints) Indices of the atoms to include for the second dimension; Default is indicesA
+		minDist: (float,Optional) Minimum distance. We set this to avoid returning a value of 0 when there is overlap between indicesA and indicesB; if you want it to return 0 in that case then just set minDist to be negative
+			 
+	Returns
+		outDist: (float) The minimum distance between atoms in indicesA and indicesB
+ 
+	"""
+	distMatrix = calcDistanceMatrixForCell_minImageConv(inpCell, indicesA=indicesA, indicesB=indicesB)
+	for rIdx, unused in enumerate(distMatrix):
+		for cIdx, unused in enumerate(distMatrix[rIdx]):
+			if distMatrix[rIdx][cIdx] < minDist:
+				distMatrix[rIdx][cIdx] = np.inf
+
+	return np.min(distMatrix)
+
+
+def calcDistanceMatrixForCell_minImageConv(inpCell, indicesA=None, indicesB=None):
+	""" Calculates the distance matrix for coords in inpCell using the nearest image convention
+	
+	Args:
+		inpCell: (plato_pylib UnitCell object)
+		indicesA: (Optional, iter of ints) Indices of the atoms to include for the first dimension; Default is to include ALL atoms
+		indicesB: (Optional, iter of ints) Indices of the atoms to include for the second dimension; Default is indicesA
+ 
+	Returns
+		 distMatrix: (NxN numpy array) distMatrix[n][m] gives the distance between atom n and m
+ 
+	"""
+	#Sort out default args
+	cartCoords = inpCell.cartCoords
+	indicesA = [x for x in range(len(cartCoords))] if indicesA is None else indicesA
+	indicesB = indicesA if indicesB is None else indicesB
+
+	#Get the coords
+	coordsA = np.array([ x[:3] for idx,x in enumerate(inpCell.cartCoords) if idx in indicesA ])
+	coordsB = np.array([ x[:3] for idx,x in enumerate(inpCell.cartCoords) if idx in indicesB ])
+
+	#Calculate the relevant distance matrix
+	dims = mdAnalysisInter.getMDAnalysisDimsFromUCellObj(inpCell)
+	distMatrix = distLib.distance_array(coordsA, coordsB, box=dims)
+	return distMatrix
 
 def calcSingleDistBetweenCoords_minImageConv(inpCell, coordA, coordB):
 	""" Gets the distance between two co-ordinates in inpCell using the minimum image convention; thus, this will return the smallest distance possible with the PBCs
--- gen_basis_helpers/analyse_md/unit_tests/utests_calc_dists.py
+++ gen_basis_helpers/analyse_md/unit_tests/utests_calc_dists.py
@@ -3,9 +3,110 @@
 import unittest
 import unittest.mock
 
+import numpy as np
+
 import plato_pylib.shared.ucell_class as uCellHelp
 
 import gen_basis_helpers.analyse_md.calc_dists as tCode
+
+
+
+class TestCalcNearestDistance(unittest.TestCase):
+
+	def setUp(self):
+		self.lattParams = [10,10,10]
+		self.lattAngles = [90,90,90]
+		self.coords = [ [5,5,5,"X"],
+		                [5,5,6,"Y"],
+		                [5,5,8,"Y"],
+		                [5,5,1,"Z"] ]
+		self.indicesA = None 
+		self.indicesB = None
+		self.minDist = 1e-2
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.cellA = uCellHelp.UnitCell(lattParams=self.lattParams, lattAngles=self.lattAngles)
+		self.cellA.cartCoords = self.coords
+
+	def _runTestFunct(self):
+		args = [self.cellA]
+		kwargs = {"indicesA":self.indicesA, "indicesB":self.indicesB, "minDist":self.minDist}
+		return tCode.calcNearestDistanceBetweenTwoSetsOfIndices(*args, **kwargs)
+
+	def testForSimpleCaseA(self):
+		expVal = 1
+		actVal = self._runTestFunct()
+		self.assertAlmostEqual(expVal,actVal)
+
+	def testForTwoDiffSetsOfIndices(self):
+		self.indicesA = [0,1]
+		self.indicesB = [2,3]
+		self.createTestObjs()
+
+		expVal = 2
+		actVal = self._runTestFunct()		
+		self.assertAlmostEqual(expVal, actVal)
+
+	def testCaseA_zeroExpected(self):
+		self.minDist = -1e-2
+		expVal = 0
+		actVal = self._runTestFunct()
+		self.assertAlmostEqual(expVal, actVal)
+
+class TestCalcDistMatrix(unittest.TestCase):
+
+	def setUp(self):
+		self.lattParams = [10,10,10]
+		self.lattAngles = [90,90,90]
+		self.coords = [ [5,5,5,"X"],
+		                [5,5,6,"Y"] ]
+		self.indicesA = None
+		self.indicesB = None
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.cellA = uCellHelp.UnitCell(lattParams=self.lattParams, lattAngles=self.lattAngles)
+		self.cellA.cartCoords = self.coords
+
+	def _runTestFunct(self):
+		kwargs = {"indicesA":self.indicesA, "indicesB":self.indicesB}
+		return tCode.calcDistanceMatrixForCell_minImageConv(self.cellA, **kwargs)
+
+	def testExpVal_pbcsIrrelevant(self):
+		expArray = np.array( ([0, 1], [1,0]) )
+		actArray = self._runTestFunct()
+		self.assertTrue( np.allclose(expArray, actArray) )
+
+	def testExpVal_pbcsMatter(self):
+		self.coords[0][-2] = 1
+		self.coords[1][-2] = 9
+		self.createTestObjs()
+		expArray = np.array( ([0,2],[2,0]) )
+		actArray = self._runTestFunct()
+		self.assertTrue( np.allclose(expArray,actArray) )
+
+	def testWithOneIndicesListPassed(self):
+		self.coords = [ [5,5,5,"X"],
+		                [5,5,6,"Y"],
+		                [5,5,7,"Z"] ]
+		self.indicesA = [0,2]
+		self.createTestObjs()
+		expArray = np.array( ([0,2],[2,0]) )
+		actArray = self._runTestFunct()
+		self.assertTrue( np.allclose(expArray,actArray) )
+
+	def testWithTwoIndicesListsPassed(self):
+		self.coords = [ [5,5,5,"X"],
+		                [5,5,6,"Y"],
+		                [5,5,7,"Z"] ]
+		self.indicesA, self.indicesB = [0,2], [0,1]
+		self.createTestObjs()
+		expArray = np.array( ([0,1], [2,1]) )
+		actArray = self._runTestFunct()
+
+		self.assertTrue( np.allclose(expArray,actArray) )
+
 
 class TestCalcSingleDistance(unittest.TestCase):
 
@@ -92,7 +193,6 @@ class TestGetNearestImageNebCoords(unittest.TestCase):
 		
 	def _runTestFunct(self):
 		kwargs = {"cartToFractMatrix":self.cartToFractMatrix, "fractToCartMatrix":self.fractToCartMatrix}
-#		return tCode.getNearestImageNebCoordsBasic(self.testCell, self.coordA, self.coordB,**kwargs) 
 		return tCode.getNearestImageNebCoordsBasic(self.testCell, self.coordA, self.coordB) 
 
 	def _checkExpAndActCoordsEqual(self, exp, act):
