cd52ce39f20e813b1ca827ef32fdf09b3bc77c61
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-05-11 11:34:21 +0100

    cp2k: Added way of specifying fixed xyz (for plane) when defining point-plane distance collective var

--- gen_basis_helpers/cp2k/collective_vars.py
+++ gen_basis_helpers/cp2k/collective_vars.py
@@ -1,4 +1,6 @@
 
+import itertools as it
+from ..shared import simple_vector_maths as vectHelp
 
 class MetaVarStandard():
 	""" Class representing a metavariable """
@@ -105,4 +107,63 @@ class DistancePointPlaneColVar_att2(CollectiveVarStandard):
 		colVar.Atom_point = 4
 		colVar.Atoms_plane = [1,2,3]
 
+class DistancePointPlaneColVar_fixedPointsForPlane(CollectiveVarStandard):
+
+	def __init__(self, atomPointIndex, planeXyzVals):
+		""" Initializer
+
+		NOTE:
+			Indices start at zero for this object. Mapping to cp2k index system (which starts at one) is handled internally
+	
+		Args:
+			atomPointIndex: (int) Index for atom defining the point
+			planeXyzVals: (iter of len-3 floats) Each is [x,y,z] and together they should define the plane
+
+		"""
+		self.atomPointIndex = atomPointIndex
+		self.planeXyzVals = planeXyzVals
+
+	def addColVarToSubsys(self, pyCp2kObj):
+		pyCp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].SUBSYS.COLVAR_add()
+		colVar = pyCp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].SUBSYS.COLVAR_list[-1].DISTANCE_POINT_PLANE
+
+		#Sort out points defining the plane
+		for idx in range(len(self.planeXyzVals)):
+			colVar.POINT_add()
+			colVar.POINT_list[-1].Type = "FIX_POINT"
+			colVar.POINT_list[-1].Xyz = self.planeXyzVals[idx]
+
+		#Add the points for the single atom point
+		colVar.POINT_add()
+		colVar.POINT_list[-1].Atoms = [self.atomPointIndex + 1]
+
+		#Tell CP2K which points are the plane and which are the single atom point
+		colVar.Atom_point = 4
+		colVar.Atoms_plane = [1,2,3]
+
+def getXyzValsFromSurfacePlaneEquationAndInpCellStandard(planeEqn, inpCell, unitLength=True):
+	""" Takes plane Equation (defining a slice through a surface, orthog to a/b) and input cell and returns 3 points in the plane
+	
+	Args:
+		planeEqn: (ThreeDimPlaneEquation object) Normal vector should be orthogonal to a/b cell vectors
+		inpCell: (plato_pylib UnitCell object) Used to get a/b vectors; which should span the plane
+		unitLength: (Bool) If True then unit translations are used to map generate two fo the three points used
+
+	Returns
+		outXyz: (iter of len-3 float iters)
+ 
+	"""
+
+	#Figure out the two translation vectors (which should lie in the plane)
+	vectA = inpCell.getLattVects()[0] 
+	vectB = inpCell.getLattVects()[1]
+	if unitLength:
+		vectA = vectHelp.getUnitVectorFromInpVector(vectA)
+		vectB = vectHelp.getUnitVectorFromInpVector(vectB)
+
+	pointA = planeEqn.getPointClosestToOrigin()
+	pointB = [ a+v1 for a,v1 in it.zip_longest(pointA,vectA) ]
+	pointC = [ a+v2 for a,v2 in it.zip_longest(pointA,vectB) ]
+
+	return [pointA, pointB, pointC]
 
--- gen_basis_helpers/cp2k/unit_tests/utests_collective_vars.py
+++ gen_basis_helpers/cp2k/unit_tests/utests_collective_vars.py
@@ -1,10 +1,13 @@
 
+import itertools as it
 import unittest
 import unittest.mock as mock
 
+import plato_pylib.shared.ucell_class as uCellHelp
+
 import gen_basis_helpers.cp2k.method_register as methReg
 import gen_basis_helpers.cp2k.collective_vars as tCode
-
+import gen_basis_helpers.shared.plane_equations as planeEqnHelp
 
 class TestMetaVarStandard(unittest.TestCase):
 
@@ -24,6 +27,34 @@ class TestMetaVarStandard(unittest.TestCase):
 		actScale = actMetaVar.Scale	
 		self.assertEqual(self.index, actIdx)
 		self.assertEqual(self.scale, actScale)
+
+
+class TestDistancePointPlaneFixedPointForPlane(unittest.TestCase):
+
+	def setUp(self):
+		self.atomPointIndex = 3
+		self.planePoints = [ [0,0,1], [2,2,1], [4,2,1] ] #integers make this simpler to deal with
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.testObjA = tCode.DistancePointPlaneColVar_fixedPointsForPlane(self.atomPointIndex, self.planePoints)
+		self.pyCp2kObj = methReg.createCP2KObjFromMethodStr("spe_standard")
+
+	def testExpectedCaseA(self):
+		self.testObjA.addColVarToSubsys(self.pyCp2kObj)
+		actColVar = self.pyCp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].SUBSYS.COLVAR_list[-1].DISTANCE_POINT_PLANE
+		
+		actColSection = self.pyCp2kObj.CP2K_INPUT.FORCE_EVAL_list[-1].SUBSYS.COLVAR_list[-1].DISTANCE_POINT_PLANE
+		self.assertEqual( [self.atomPointIndex+1], actColSection.POINT_list[3].Atoms )
+
+		# Deal with the plane
+		for idx in range(3):
+			self.assertEqual( "FIX_POINT", actColSection.POINT_list[idx].Type )
+			self.assertEqual( self.planePoints[idx], actColSection.POINT_list[idx].Xyz )
+
+		#Combine all
+		self.assertEqual( 4, actColSection.Atom_point )
+		self.assertEqual( [1,2,3], actColSection.Atoms_plane )
 
 
 class TestDistancePointPlaneColVarAtt2(unittest.TestCase):
@@ -71,6 +102,43 @@ class TestDistancePointPlaneColVar(unittest.TestCase):
 		expPlaneIndices = [x+1 for x in self.atomPlaneIndices]
 		self.assertEqual( expPointIdx, actColVar.Atom_point )
 		self.assertEqual( expPlaneIndices, actColVar.Atoms_plane )
+
+
+class TestGetXyzValsFromSurfPlaneEquation(unittest.TestCase):
+
+	def setUp(self):
+		self.lattParams, self.lattAngles = [8,9,10], [90,90,90]
+		self.d = 3
+		self.unitLength = True
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.cellA = uCellHelp.UnitCell(lattParams=self.lattParams, lattAngles=self.lattAngles)
+		self.planeEqn = planeEqnHelp.ThreeDimPlaneEquation(0,0,1,self.d) #Our normal points along z
+
+	def _runTestFunct(self):
+		args = [self.planeEqn, self.cellA]
+		kwargs = {"unitLength":self.unitLength}
+		return tCode.getXyzValsFromSurfacePlaneEquationAndInpCellStandard(*args, **kwargs)
+
+	def testExpectedValSimpleCaseA(self):
+		expVals = [ [0,0,self.d], [1,0,self.d], [0,1,self.d] ]
+		actVals = self._runTestFunct()
+		self.checkExpAndActValsMatch(expVals, actVals)
+
+	def testExpectedNonUnitLength(self):
+		expVals = [ [0,0,self.d], [8,0,self.d], [0,9,self.d] ]
+		self.unitLength = False
+		actVals = self._runTestFunct()
+		self.checkExpAndActValsMatch(expVals, actVals)
+
+	def checkExpAndActValsMatch(self, expVals, actVals):
+		self.assertEqual( len(expVals), len(actVals) )
+
+		for expCoord, actCoord in zip(expVals, actVals):
+			[self.assertAlmostEqual(e,a) for e,a in it.zip_longest(expCoord, actCoord)]
+
+
 
 
 
