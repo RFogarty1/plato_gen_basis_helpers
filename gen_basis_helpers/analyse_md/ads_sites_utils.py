8ef9677fd3ce138ccb2535052ca5ca857268ce64
Richard M Fogarty <richard.m.fogarty@gmail.com>
2021-06-08 17:01:39 +0100

    analyse_md: Added function to match adsorbate sites and adsorption atoms
    Also some convenience function to help plotting occupancy of adsorption sites from point of view of adsorabte and the ads site

--- gen_basis_helpers/analyse_md/ads_sites_utils.py
+++ gen_basis_helpers/analyse_md/ads_sites_utils.py
@@ -1,5 +1,8 @@
 
 import copy
+import itertools as it
+
+import numpy as np
 
 from . import calc_dists as calcDistHelp
 from ..shared import cart_coord_utils as cartHelp
@@ -28,6 +31,155 @@ class AddAdsSitesToGeomsStandard():
 		inpCell.cartCoords = cartCoords + newCoords
 
 
+def getAssignedAdsIndiceForTrajectory(inpTraj, adsSiteObjs, inpIndices, maxHozDist=2, maxTotDist=None):
+	""" Runs "assignAdsIndicesToIndividualAdsorptionSites" for each step in inpTraj and returns the output
+	
+	Args:
+		inpTraj: (TrajectoryInMemory) Contains all info for a simulation
+		other args: See assignAdsIndicesToIndividualAdsorptionSites for description
+
+	Returns
+		assignedAdsIndicesForTraj: (One output per step) Each element is an iter of iters; For each step each element corresponds to one index in adsSiteObjs. The value of each of these elements is a list of indices showing the adsorbates assigned to that site
+
+ 
+	"""
+	sharedArgs = [adsSiteObjs, inpIndices]
+	sharedKwargs = {"maxHozDist":maxHozDist, "maxTotDist":maxTotDist}
+	outObjs = list()
+	for step in inpTraj:
+		currCell = step.unitCell
+		currData = assignAdsIndicesToIndividualAdsorptionSites(currCell, *sharedArgs, **sharedKwargs)
+		outObjs.append(currData)
+	return outObjs
+
+
+def getPlotDataFromAssignedIndices_adsSiteOccupiedOverTime(inpTimes, assignedAdsIndices):
+	""" Function to get data to plot showing when adsorbate sites are occupied (xVals are site indices, y-values are times)
+	
+	Args:
+		inpTimes: (iter of floats) The time for each snapshot. Generally get from [x.time for x in traj]
+		assignedAdsIndices: (iter of iter of iters) The output of "getAssignedAdsIndiceForTrajectory". Each element is for one step of the trajectory, each element of that contains a list of adsorbate atoms assigned to the adosrbate site of that index. e.g. assignedAdsIndices[stepIdx][adsSiteIdx] contains the atomic indices assigned to adsSiteIdx and time corresponding to stepIdx
+			 
+	Returns
+		plotData: (iter of iter of len-2 iters) Each element contains data to plot for one of the adsorbate sites. In each case this means nx2 data; where 1st column is just the index of the adsorption site (i.e. the same for each data point) while the 2nd column contains either the time of this step (if occupied) or np.nan (if not occupied)
+ 
+	Raises:
+		AssertionError: If len(inpTimes)!=len(assignedAdsIndices)
+
+	"""
+
+	assert len(inpTimes)==len(assignedAdsIndices), "len(inpTimes)={}, len(assignedAdsIndices)={}".format( len(inpTimes),len(assignedAdsIndices) )
+
+	outData = [list() for x in range(len(assignedAdsIndices[0]))]
+	for time, assignedData in it.zip_longest(inpTimes, assignedAdsIndices):
+		for adsSiteIdx,unused in enumerate(assignedData):
+			if len(assignedData[adsSiteIdx]) == 0:
+				outData[adsSiteIdx].append( [adsSiteIdx, np.nan] )
+			elif len(assignedData[adsSiteIdx]) > 0:
+				outData[adsSiteIdx].append( [adsSiteIdx,time] )
+			else:
+				raise ValueError("Shouldnt ever trigger this")
+
+	return outData
+
+
+def getPlotDataFromAssignedIndices_adsorbatesAdsorbedOverTime(inpTimes, assignedAdsIndices):
+	""" Function to get data to plot showing when adsorbates are adsorbed at ANY site (x-values are adsorbate indices, y-values are times)
+	
+	Args:
+		inpTimes: (iter of floats) The time for each snapshot. Generally get from [x.time for x in traj]
+		assignedAdsIndices: (iter of iter of iters) The output of "getAssignedAdsIndiceForTrajectory". Each element is for one step of the trajectory, each element of that contains a list of adsorbate atoms assigned to the adosrbate site of that index. e.g. assignedAdsIndices[stepIdx][adsSiteIdx] contains the atomic indices assigned to adsSiteIdx and time corresponding to stepIdx
+			 
+	Returns
+		plotData: (iter of iter of len-2 iters) Each element contains data to plot for one of the adsorbate sites. In each case this means nx2 data; where 1st column is just the index of the adsorbate (i.e. the same for each data point) while the 2nd column contains either the time of this step (if adsorbed) or np.nan (if not adsorbed)
+
+	"""
+	mappedData = _getAssignedAdsIndicesInAdsorbingAtomCentricForm(assignedAdsIndices)
+	outData = getPlotDataFromAssignedIndices_adsSiteOccupiedOverTime(inpTimes, mappedData)
+	return outData
+
+def _getAssignedAdsIndicesInAdsorbingAtomCentricForm(assignedAdsIndices):
+
+	#First figure out ALL the inpIndices that ever adsorb to something
+	allIndices = set()
+	for assIndices in assignedAdsIndices:
+		currAdsorbateIndices = set(it.chain(*assIndices))
+		allIndices.update(currAdsorbateIndices)
+
+	#Now map the original indices to the output indices
+	outputIndices = [idx for idx in range(len(allIndices))]
+	mapInpToOutputIndices = { key:idx for idx,key in enumerate( sorted(list(allIndices)) ) }
+
+	#Now create the output data
+	outData = list()
+	for stepIdx, assIndices in enumerate(assignedAdsIndices):
+		currData = [list() for x in range(len(allIndices))]
+		for adsSiteIdx, adsIndices in enumerate( assIndices ):
+			mappedAdsIndices = [mapInpToOutputIndices[x] for x in adsIndices] #len-0 or len-1 generally
+			for mappedIdx in mappedAdsIndices:
+				currData[mappedIdx].append(adsSiteIdx)
+		outData.append(currData)
+	
+	return outData
+
+
+#Mainly test this by calling from "assignAdsIndicesToAdsorptionSites"
+def assignAdsIndicesToIndividualAdsorptionSites(inpCell, adsSiteObjs, inpIndices, maxHozDist=2, maxTotDist=None):
+	""" For each adsSite, assign a list of inpIndices which can be assigned to that site (length=0 or 1 generally expected)
+	
+	Args:
+		inpCell: (plato_pylib UnitCell object)
+		adsSiteObjs: (iter of FixedIndicesAdsSiteBase objects) Each of these represents an adsorption site
+		inpIndices: (iter of ints) Indices of adsorbate atoms in inpCell (we want to assign THESE to adsorption sites)
+		maxHozDist: (float) Maximum horizontal (in-plane) distance from adsorption site to adsorabte atom
+		maxTotDist: (float, Optional) Maximum total distance from adsorption site to adsorbate atom. Setting to None means maximum distance not taken into account
+			 
+	Returns
+		 siteVsAds: (iter of iters) Each element corresponds to one index in adsSiteObjs. The value of each element is a list of indices showing the adsorbates assigned to that site
+ 
+	"""
+
+	#Get (PBC-aware) total distance and horizontal distance matrices between adsSiteObjs and inpIndices
+	inpCellCartCoords = inpCell.cartCoords #want to save accesing this multiple times
+	useCell = copy.deepcopy(inpCell)
+	nCoords = len(useCell.fractCoords)
+	adsSiteCoords = [ site.positionFromGeom(useCell, inpCartCoords=inpCellCartCoords) + ["ads_site"] for site in adsSiteObjs ]
+	useCell.cartCoords = useCell.cartCoords + adsSiteCoords
+	firstAdsSiteIdx = len(inpCell.cartCoords)
+	adsSiteIndices = [x for x in range(firstAdsSiteIdx,firstAdsSiteIdx+len(adsSiteCoords))]
+
+	distMatrix = calcDistHelp.calcDistanceMatrixForCell_minImageConv(useCell, indicesA=inpIndices, indicesB=adsSiteIndices)
+	hozDistMatrix = calcDistHelp.calcHozDistMatrixForCell_minImageConv(useCell, indicesA=inpIndices, indicesB=adsSiteIndices)	
+
+
+	#
+	cartCoords = useCell.cartCoords
+	surfPlaneEqn = cartHelp.getABPlaneEqnWithNormVectorSameDirAsC_uCellInterface(useCell)
+	outLists = [list() for x in adsSiteObjs]
+
+
+	for mappedInpIdx,inpIdx in enumerate(inpIndices):
+		currPos = cartCoords[inpIdx][:3]
+		minHozDist, currSiteIdx = maxHozDist, None
+		for adsIdx,adsPos in enumerate(adsSiteCoords):
+			currHozDist = hozDistMatrix[mappedInpIdx][adsIdx]
+			if currHozDist < minHozDist:
+				#Check maximum distance criterion is satisfied
+				if maxTotDist is None:
+					currSiteIdx = adsIdx
+					minHozDist = currHozDist
+				else:
+					totDist = distMatrix[mappedInpIdx][adsIdx]
+					if totDist <= maxTotDist:
+						currSiteIdx = adsIdx
+						minHozDist = currHozDist
+
+		if currSiteIdx is not None:
+			outLists[currSiteIdx].append(inpIdx)
+
+	return outLists
+
+
 def assignAdsIndicesToAdsorptionSites(inpCell, adsSiteObjs, inpIndices, maxHozDist=2, maxTotDist=None):
 	""" For each unique sitePosition label (defined in adsObjs) this function returns a list of atom indices which can be associated with that site
 	
@@ -50,42 +202,18 @@ def assignAdsIndicesToAdsorptionSites(inpCell, adsSiteObjs, inpIndices, maxHozDi
 	assert "None" not in outDict.keys()
 	outDict["None"] = list()
 
+	#Assign vals into indices
+	adsorbatesForIdx = assignAdsIndicesToIndividualAdsorptionSites(inpCell, adsSiteObjs, inpIndices, maxHozDist=maxHozDist, maxTotDist=maxTotDist)
+	for idx, vals in enumerate(adsorbatesForIdx):
+		currSite = adsSiteObjs[idx].siteName
+		for val in vals:
+			outDict[currSite].append(val)
 
-	#Get (PBC-aware) total distance and horizontal distance matrices between adsSiteObjs and inpIndices
-	inpCellCartCoords = inpCell.cartCoords #want to save accesing this multiple times
-	useCell = copy.deepcopy(inpCell)
-	nCoords = len(useCell.fractCoords)
-	adsSiteCoords = [ site.positionFromGeom(useCell, inpCartCoords=inpCellCartCoords) + ["ads_site"] for site in adsSiteObjs ]
-	useCell.cartCoords = useCell.cartCoords + adsSiteCoords
-	firstAdsSiteIdx = len(inpCell.cartCoords)
-	adsSiteIndices = [x for x in range(firstAdsSiteIdx,firstAdsSiteIdx+len(adsSiteCoords))]
-
-	distMatrix = calcDistHelp.calcDistanceMatrixForCell_minImageConv(useCell, indicesA=inpIndices, indicesB=adsSiteIndices)
-	hozDistMatrix = calcDistHelp.calcHozDistMatrixForCell_minImageConv(useCell, indicesA=inpIndices, indicesB=adsSiteIndices)	
-
-
-	#
-	cartCoords = useCell.cartCoords
-	surfPlaneEqn = cartHelp.getABPlaneEqnWithNormVectorSameDirAsC_uCellInterface(useCell)
-#	adsSitePositions = [x.positionFromGeom(useCell, inpCartCoords=cartCoords) for x in adsSiteObjs]
-	for mappedInpIdx,inpIdx in enumerate(inpIndices):
-		currPos = cartCoords[inpIdx][:3]
-		minHozDist, currSiteName = maxHozDist, "None"
-		for adsIdx,adsPos in enumerate(adsSiteCoords):
-			currHozDist = hozDistMatrix[mappedInpIdx][adsIdx]
-			if currHozDist < minHozDist:
-				#Check maximum distance criterion is satisfied
-				if maxTotDist is None:
-					currSiteName = adsSiteObjs[adsIdx].siteName
-					minHozDist = currHozDist
-				else:
-					totDist = distMatrix[mappedInpIdx][adsIdx]
-					if totDist <= maxTotDist:
-						currSiteName = adsSiteObjs[adsIdx].siteName
-						minHozDist = currHozDist
-
-		outDict[currSiteName].append(inpIdx)
+	#Figure out the ones with None
+	assignedIndices = [ x for x in it.chain( *[outDict[key] for key in outDict.keys()] ) ]
+	outDict["None"] = [ x for x in inpIndices if x not in assignedIndices ]
 
 	return outDict
+
 
 
--- gen_basis_helpers/analyse_md/unit_tests/utests_ads_sites_utils.py
+++ gen_basis_helpers/analyse_md/unit_tests/utests_ads_sites_utils.py
@@ -1,13 +1,184 @@
 
 import copy
+import itertools as it
 import math
 import unittest
 import unittest.mock as mock
 
+import numpy as np
+
 import plato_pylib.shared.ucell_class as uCellHelp
 
 import gen_basis_helpers.analyse_md.ads_sites_impl as adsSiteImplHelp
+import gen_basis_helpers.analyse_md.traj_core as trajCoreHelp
 import gen_basis_helpers.analyse_md.ads_sites_utils as tCode
+
+
+
+class TestGetPlotDataFromAssignedIndices_adsAtomsAdsorbedOverTime(unittest.TestCase):
+
+	def setUp(self):
+		self.inpTimes = [1,2]
+		self.indicesTimeA = [ [] , [20], [3]  ] #3 adsorption sites, and 4 adsorbates
+		self.indicesTimeB = [ [2], [5]  , [20] ]
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.assignedIndices = [self.indicesTimeA, self.indicesTimeB]
+
+	def testMapToAdsorptionCentricA(self):
+		expIndicesTimeA = [ [] , [2], [] , [1] ] #Order is by idx. So [2,3,5,20]
+		expIndicesTimeB = [ [0], [] , [1], [2] ] 
+		expAssignedIndices = [expIndicesTimeA, expIndicesTimeB]
+		actAssignedIndices = tCode._getAssignedAdsIndicesInAdsorbingAtomCentricForm(self.assignedIndices)
+		self.assertEqual(expAssignedIndices, actAssignedIndices)
+
+	def testExpectedPlotDataReturned(self):
+		timeA, timeB = self.inpTimes
+		expData = [ [[0, np.nan], [0, timeB]] ,
+		            [[1, timeA] , [1, np.nan]],
+		            [[2, np.nan], [2, timeB]],
+		            [[3, timeA] , [3, timeB]] ]
+
+		actData = tCode.getPlotDataFromAssignedIndices_adsorbatesAdsorbedOverTime(self.inpTimes, self.assignedIndices)
+		self._checkExpAndActDataEqual(expData, actData)
+
+	def _checkExpAndActDataEqual(self, expData, actData):
+		self.assertEqual( len(expData), len(actData) )
+		for expSeries, actSeries in it.zip_longest(expData, actData):
+			self.assertEqual( len(expSeries), len(actSeries) )
+			for exp, act in it.zip_longest(expSeries, actSeries):
+				self.assertAlmostEqual(exp[0],act[0])
+				if (exp[1] is np.nan) and (act[1] is np.nan):
+					pass
+				else:
+					self.assertAlmostEqual(exp[1],act[1])
+
+
+
+class TestGetPlotDataFromAssignedIndices_adsSiteOccupiedOverTime(unittest.TestCase):
+
+	def setUp(self):
+		self.inpTimes = [1,2]
+		self.indicesTimeA = [ [] , [2], [] ] 
+		self.indicesTimeB = [ [1], [] , [] ]
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.assignedIndices = [self.indicesTimeA, self.indicesTimeB]
+
+	def _runTestFunct(self):
+		args = [self.inpTimes, self.assignedIndices]
+		return tCode.getPlotDataFromAssignedIndices_adsSiteOccupiedOverTime(*args)
+
+	def testExpectedCaseA(self):
+		timeA, timeB = self.inpTimes
+
+		expData = [  [[0,np.nan], [0, timeB]], #0th adsorption site
+		             [[1, timeA], [1,np.nan]],
+		             [[2,np.nan], [2,np.nan]] ]
+
+		actData = self._runTestFunct()
+		self._checkExpAndActDataEqual(expData, actData)
+
+
+	def testRaisesIfTimesLengthDoesntMatchAssignedLength(self):
+		self.inpTimes.append(4)
+		with self.assertRaises(AssertionError):
+			self._runTestFunct()
+
+	def _checkExpAndActDataEqual(self, expData, actData):
+		self.assertEqual( len(expData), len(actData) )
+		for expSeries, actSeries in it.zip_longest(expData, actData):
+			self.assertEqual( len(expSeries), len(actSeries) )
+			for exp, act in it.zip_longest(expSeries, actSeries):
+				self.assertAlmostEqual(exp[0],act[0])
+				if (exp[1] is np.nan) and (act[1] is np.nan):
+					pass
+				else:
+					self.assertAlmostEqual(exp[1],act[1])
+
+
+class TestGetAssignedAdsIndicesForTrajectory(unittest.TestCase):
+
+	def setUp(self):
+		self.steps = [1,2]
+		self.uCells = [mock.Mock(),mock.Mock()]
+		self.adsSiteObjs = mock.Mock()
+		self.inpIndices = mock.Mock()
+		self.maxHozDist = 5
+		self.maxTotDist = 10
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.trajStepA = trajCoreHelp.TrajStepBase(unitCell=self.uCells[0], step=self.steps[0])
+		self.trajStepB = trajCoreHelp.TrajStepBase(unitCell=self.uCells[1], step=self.steps[1])
+		self.trajObjA = trajCoreHelp.TrajectoryInMemory([self.trajStepA, self.trajStepB])
+
+	def _runTestFunct(self):
+		args = [self.trajObjA, self.adsSiteObjs, self.inpIndices]
+		kwargs = {"maxHozDist":self.maxHozDist, "maxTotDist":self.maxTotDist}
+		return tCode.getAssignedAdsIndiceForTrajectory(*args, **kwargs)
+
+	@mock.patch("gen_basis_helpers.analyse_md.ads_sites_utils.assignAdsIndicesToIndividualAdsorptionSites")
+	def testExpectedCallsA(self, mockAssignOneGeom):
+		expOutput = [mock.Mock(), mock.Mock()]
+		mockAssignOneGeom.side_effect = expOutput
+
+		actOutput = self._runTestFunct()
+
+		#Check expected called
+		sharedKwargs = {"maxHozDist":self.maxHozDist, "maxTotDist":self.maxTotDist}
+		mockAssignOneGeom.assert_any_call(self.uCells[0], self.adsSiteObjs, self.inpIndices, **sharedKwargs)
+		mockAssignOneGeom.assert_called_with(self.uCells[1], self.adsSiteObjs, self.inpIndices, **sharedKwargs)
+
+		#Check expected output
+		self.assertEqual(expOutput, actOutput)
+
+
+
+class TestAssignAdsIndicesToIndividualAdsorptionSites(unittest.TestCase):
+
+	def setUp(self):
+		self.adsSiteIndices = [1,2]
+		self.adsIndices = [0]
+		self.maxHozDist = 4 #If somethings further than this its considered to be "None"
+		self.siteName = "site_a"
+		self.maxTotDist = None
+		self.createTestObjs()
+
+	def createTestObjs(self):
+		self.adsObjs = [adsSiteImplHelp.TopStandard(x, siteName=self.siteName) for x in self.adsSiteIndices]
+		self.cellA = loadTestCellA()
+		cartCoords = [ [9,9,9,"X"],
+		               [1,1,1,"Y"],
+		               [6,6,5,"Z"] ]
+		self.cellA.cartCoords = cartCoords	
+
+	def _runTestFunct(self):
+		args = [self.cellA, self.adsObjs, self.adsIndices]
+		kwargs = {"maxHozDist":self.maxHozDist, "maxTotDist":self.maxTotDist}
+		return tCode.assignAdsIndicesToIndividualAdsorptionSites(*args, **kwargs)
+
+	def testSimpleCaseA(self):
+		expOutput = [ [0], [] ]
+		actOutput = self._runTestFunct()
+		self.assertEqual(expOutput, actOutput)
+
+	#Just flip the indices around; will make sure we dont just use indices within adsIndices rather than cartCoords for example
+	def testSimpleCaseB(self):
+		self.adsSiteIndices = [2,0]
+		self.adsIndices = [1]
+		self.createTestObjs()
+		expOutput = [ [], [1] ]
+		actOutput = self._runTestFunct()
+		self.assertEqual(expOutput, actOutput)
+
+	def testExpectedWhenNoAdsorbateInRange(self):
+		self.maxHozDist = 1
+		expOutput = [ [], [] ]
+		actOutput = self._runTestFunct()
+		self.assertEqual( expOutput, actOutput )
 
 
 class TestAssignAdsIndicesToAdsorptionSites(unittest.TestCase):
@@ -33,7 +204,6 @@ class TestAssignAdsIndicesToAdsorptionSites(unittest.TestCase):
 		expDict = {"None":list(), "site_a":[0]}
 		actDict = self._runTestFunct()
 		self.assertEqual(expDict, actDict)
-
 
 	def testExpectedCaseB(self):
 		self.maxHozDist = 2 #sqrt(8) is the hoz dist for the None site
