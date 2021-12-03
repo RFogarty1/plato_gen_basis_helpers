analyse_md: Bug fix for water derivative counter
It wasnt using the minDist attribute

gen_basis_helpers/analyse_md/water_derivative_species_classifiers.py
@@ -10,7 +10,7 @@ from . import classifier_objs as clasObjsBase
 
 class _WaterDerivativeDistanceOnlyClassifierGeneric(clasObjsBase.ClassifierBase):
 
-	def __init__(self, oxyIndices, hyIndices, maxOHDist=1.3, nNebs=2):
+	def __init__(self, oxyIndices, hyIndices, maxOHDist=1.3, nNebs=2, execCount=0):
 		""" Initializer
 		
 		Args:
@@ -18,13 +18,14 @@ class _WaterDerivativeDistanceOnlyClassifierGeneric(clasObjsBase.ClassifierBase)
 			hyIndices: (iter of ints) All hydrogen atoms you want to check for being part of the water derivative
 			maxDistOH: (float) Maximum distance between O-H for the hydrogen to be considered bonded to the OH 
 			nNebs: (int) The number of hydrogen that need to be in range (e.g. 3 for hydronium, 2 for water, 1 for hydroxyl)
+			execCount: (int) Used to track how many times .classify is called; used as a safety check when using "byReference" classifiers
 
 		"""
 		self.oxyIndices = oxyIndices
 		self.hyIndices = hyIndices
 		self.maxOHDist = maxOHDist
 		self.nNebs = nNebs 
-
+		self.execCount = execCount
 
 	def classify(self, sparseMatrixCalculator):
 		oxyIndices,hyIndices = self._assignAllRelevantHydrogenToOxygens(sparseMatrixCalculator)
@@ -34,6 +35,9 @@ class _WaterDerivativeDistanceOnlyClassifierGeneric(clasObjsBase.ClassifierBase)
 			if len(hyIdxs)==self.nNebs:
 				outIndices[0].append(oxyIdxs)
 				outIndices[1].append(hyIdxs)
+
+		self.storedClassifyResult = outIndices
+		self.execCount += 1
 
 		return outIndices
 
@@ -49,7 +53,7 @@ class _WaterDerivativeDistanceOnlyClassifierGeneric(clasObjsBase.ClassifierBase)
 		distMatrix = sparseMatrixCalculator.outDict["distMatrix"]
 		for hyIdx in self.hyIndices:
 			currDists = distMatrix[hyIdx][np.array(self.oxyIndices)] #Worried about this but...
-			if np.min(currDists):
+			if np.min(currDists) < self.maxOHDist:
 				currIdx = np.argmin(currDists)
 				outHyList[currIdx].append(hyIdx)
 